3eecc27b9e9fc40e8ed4d2a5a82fe26da9d1cf66
bosmeeuw <bosmeeuw@aedb5d07-8755-0410-b22e-9704cff1a143>
2008-09-02 19:56:41 +0000

    Changes to CSS code, implement drag & drop to subfolder of a bookmarked folder

--- trunk/defaults/preferences/quickfolders.js
+++ trunk/defaults/preferences/quickfolders.js
@@ -5,3 +5,4 @@ pref("extensions.quickfolders.showUnreadFoldersBold",true);
 pref("extensions.quickfolders.useKeyboardShortcuts",true);
 pref("extensions.quickfolders.showTotalNumber",false);
 pref("extensions.quickfolders.showFoldersWithMessagesItalic",false);
+pref("extensions.quickfolders.showFlatStyle",true);

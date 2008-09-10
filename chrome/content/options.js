32fc455afbed75dc5e357cc41962f56e5f361b1b
Axel.Grude <Axel.Grude@aedb5d07-8755-0410-b22e-9704cff1a143>
2008-09-10 15:45:44 +0000

    Added Quickhelp in options window; Version number is currently hard coded (0.9) so this needs to be revisited for next release

--- chrome/content/options.js
+++ chrome/content/options.js
@@ -1,7 +1,27 @@
+const QF_CC = Components.classes;
+const QF_CI = Components.interfaces;
+
+
+function QF_getAddon(aId) {
+  return QF_CC["@mozilla.org/extensions/manager;1"]
+       .getService(QF_CI.nsIExtensionManager)
+       .getItemForID(aId);
+}
+
+function QF_getMyVersion() {
+  return QF_getAddon("{3550f703-e582-4d05-9a08-453d09bdfdc6}").version;
+}
+
 var QuickFoldersOptions = {
-    
     accept: function() {
         var observerService = Components.classes["@mozilla.org/observer-service;1"].getService(Components.interfaces.nsIObserverService);
         observerService.notifyObservers(null, "quickfolders-options-saved", null);
+    } ,
+    load : function() {
+	    var version=QF_getMyVersion();
+		//alert ("Version=" + version);
+		if (version=="") version='0.9';
+	     document.getElementById("qf-options-header-description").setAttribute("value", version);
     }
 }
+

19eb1446bfa5c8c7e8ac8a8855f39cac176db3b9
Axel.Grude <Axel.Grude@aedb5d07-8755-0410-b22e-9704cff1a143>
2009-01-14 22:45:01 +0000

    3 Tabs are better than 2! Planned: various color customization. (I need some boilerplate code for modifying the CSS styles on the fly!)

--- trunk/chrome/content/options.js
+++ trunk/chrome/content/options.js
@@ -21,6 +21,22 @@ var QuickFoldersOptions = {
 	    var version=QF_getMyVersion();
 		if (version=="") version='version?';
 	    document.getElementById("qf-options-header-description").setAttribute("value", version);
+    },
+    
+    setElementColor: function(element, col) {
+	    // alert("setElementColor!");
+	    alert("Set color of css class " + element + " to " + col);
+	    // to do: find elements of this class and change their color
+	    // find the class element itself and change its properties
+	    // persist in options
+	    // load on startup
+	    return true;
+	    // planned for later: 
+	    //            add default color pick; 
+	    //            add system colors such as ButtonFace, Highlight, HighlightText etc.
+	    // even later: 
+	    //            extend the color picker to select 2 colors and a gradient direction (vertical / horizontal)
     }
+    
 }
 

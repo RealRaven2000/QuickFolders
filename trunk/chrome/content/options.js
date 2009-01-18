9b1b667b90f960a602e3972516fe68ade07aec5b
Axel.Grude <Axel.Grude@aedb5d07-8755-0410-b22e-9704cff1a143>
2009-01-18 00:06:39 +0000

    test code for modifying style rules (colors)

--- trunk/chrome/content/options.js
+++ trunk/chrome/content/options.js
@@ -23,13 +23,95 @@ var QuickFoldersOptions = {
 	    document.getElementById("qf-options-header-description").setAttribute("value", version);
     },
     
-    setElementColor: function(element, col) {
+    getMyStyleSheet: function() {
+	  var styleSheetList = document.styleSheets;
+
+      for (var i=0; i<document.styleSheets.length; i++) {
+	    var ss = document.styleSheets[i];
+	    if (ss.title == "qfStyles") {
+	      return ss;
+        }
+      }
+      //return NaN;
+    },
+    
+    setElementColor: function(rule, colortype, color) {
 	    // alert("setElementColor!");
-	    alert("Set color of css class " + element + " to " + col);
+	    // alert("Set color of css class " + rule + " to " + color);
 	    // to do: find elements of this class and change their color
 	    // find the class element itself and change its properties
 	    // persist in options
 	    // load on startup
+	    
+	    var ss = this.getMyStyleSheet();
+	    if (!ss || ss==null)
+	      return;
+	    
+	    var rulesList=ss.cssRules; //  ? ss.cssRules : ss.rules
+	    var i;
+	    var colorRule;
+	    var RuleName = '#QuickFolders-Toolbar' + rule;
+	    alert("Search Rule Name:" + RuleName +"\n" + "rulesList.length=" + rulesList.length);
+	    var rAtoms=RuleName.split(" ");
+	    for (i=1; i<rulesList.length; i++) 
+	    { 
+		    var found;
+	        //alert("searching rule (" + i + "): " + rulesList[i].selectorText);
+		    for (var j=0; j<r.length; j++) {
+			  found=true;
+			  
+		      if (-1 == rulesList[i].selectorText.indexOf(rAtoms[j])) {
+		        found=false;
+		        break;
+	          }
+	        }
+	        if (found) {
+			  alert("Matched Rule(" + i + ")\n\nselectorText:" + rulesList[i].selectorText + "\n"+
+			        "rule type:" + rulesList[i].type + "\n" +
+			        "rule cssText:" + rulesList[i].cssText + "\n" +
+			        "setting " + colortype + ":" + color);
+			  var st=rulesList[i].style; // readonly  CSSStyleDeclaration  
+			  alert("getPropertyValue(background):" + st.getPropertyValue("background"));
+			  alert("getPropertyValue(background-color):" + st.getPropertyValue("background-color"));
+			  var k;
+			  for (k=0;k<st.length;k++) {
+			    if (colortype==st.item(k)) {
+			      alert("item(" + k  + "):" + st.item(k));
+				  rulesList[k].style.setProperty(colortype,color,"important");
+				  alert("changed? getPropertyValue(background):" + rulesList[k].style.getPropertyValue("background"));
+			    }
+		      }
+		      break;
+		      //st.removeProperty(colortype);
+			  //st.setProperty(colortype,color,"!important");
+			  
+			  
+			  //rulesList[i].style.setProperty(colortype, color, "!important");
+			  
+			  
+			  // now find the style declaration to modify
+			  /*
+			  var sStyleTextList="";
+			  for (i=0; i<st.length; i++) {
+				 sStyleTextList += st.DOMString[i] + "\n"
+			  }
+			  alert ("cssText list:" + sStyleTextList );
+			  */
+				  /*  
+				  if(rulesList[i].selectorText.toLowerCase()==RuleName.toLowerCase())
+				  { //find "a:hover" rule
+		            colorRule=rulesList[i];
+		            alert("found rule:" +colorRule.cssText);
+		            break;
+		          }
+		          */
+	        }
+		    
+	    }
+	    
+        //ss.deleteRule(colorRule);
+	    //ss.insertRule(colorRule, ss.cssRules.length);
+	    
 	    return true;
 	    // planned for later: 
 	    //            add default color pick; 

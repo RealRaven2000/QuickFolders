70277c9ac1ace77b2e816a26db6d53ccb56abab0
Axel.Grude <Axel.Grude@aedb5d07-8755-0410-b22e-9704cff1a143>
2009-01-24 11:43:29 +0000

    added generic method for persisting user styles

--- chrome/content/quickfolders-preferences.js
+++ chrome/content/quickfolders-preferences.js
@@ -62,5 +62,29 @@ QuickFolders.Preferences = {
 
     getLastSelectedCategory: function() {
         return this.service.getCharPref("extensions.quickfolders.lastSelectedCategory")
+    },
+    
+    getUserStyle: function(sId, sType) {
+	    // note: storing color as string in order to store OS specific colors like Menu, Highlight
+	    // usage: getUserStyle("ActiveTab","color")
+	    // usage: getUserStyle("ActiveTab","background-color")
+	    var sStyleName = "QuickFolders." & sId & ".Style." & sType;
+        if(!this.service.prefHasUserValue(sStyleName)) {
+            return "";
+        }
+        
+        if((folders = this.service.getCharPref(sStyleName))) {
+	        return this.service.getCharPref(sStyleName);
+        }
+        else {
+            return "";
+        }
+    },
+
+    setUserStyle: function(sId, sType, sValue) {
+	    var sStyleName = "QuickFolders." & sId & ".Style." & sType;
+        this.service.setCharPref(sStyleName, sValue);
+	    
     }
+    
 }

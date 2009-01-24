ca160162b94c03ec1430ce4384b0583f80b4f0f0
Axel.Grude <Axel.Grude@aedb5d07-8755-0410-b22e-9704cff1a143>
2009-01-24 19:16:16 +0000

    support for user set colours

--- chrome/content/options.js
+++ chrome/content/options.js
@@ -15,6 +15,31 @@ var QuickFoldersOptions = {
     accept: function() {
         var observerService = Components.classes["@mozilla.org/observer-service;1"].getService(Components.interfaces.nsIObserverService);
         observerService.notifyObservers(null, "quickfolders-options-saved", null);
+	    // persist colors
+	    try {
+		    QuickFolders.Preferences.setUserStyle("ActiveTab","background-color",
+		                                          document.getElementById("activetab-colorpicker").color);
+		    QuickFolders.Preferences.setUserStyle("ActiveTab","color",
+		                                          document.getElementById("activetab-fontcolorpicker").color);
+		                                          
+		    QuickFolders.Preferences.setUserStyle("HoveredTab","background-color",
+		                                          document.getElementById("hover-colorpicker").color); 
+		    QuickFolders.Preferences.setUserStyle("HoveredTab","color",
+		                                          document.getElementById("hover-fontcolorpicker").color); 
+		                                          
+		    QuickFolders.Preferences.setUserStyle("DragTab","background-color", 
+		                                          document.getElementById("dragover-colorpicker").color);
+		    QuickFolders.Preferences.setUserStyle("DragTab","color", 
+		                                          document.getElementById("dragover-fontcolorpicker").color);
+		                                          
+		    QuickFolders.Preferences.setUserStyle("Toolbar","background-color", 
+		                                          document.getElementById("toolbar-colorpicker").color);
+		                                          
+
+	    }
+	    catch(e) { 
+		    alert("Error in QuickFolders:\n" + e); 
+		};
     } ,
     load : function() {
 	    var version=QF_getMyVersion();
@@ -22,12 +47,21 @@ var QuickFoldersOptions = {
 	    document.getElementById("qf-options-header-description").setAttribute("value", version);
 	    // initialize colorpickers
 	    try {
+		    document.getElementById("activetab-colorpicker").color=QuickFolders.Preferences.getUserStyle("ActiveTab","background-color","HighLight");
+		    document.getElementById("activetab-fontcolorpicker").color=QuickFolders.Preferences.getUserStyle("ActiveTab","color","HighlightText");
+
+		    document.getElementById("hover-colorpicker").color=QuickFolders.Preferences.getUserStyle("HoveredTab","background-color","Orange");
+		    document.getElementById("hover-fontcolorpicker").color=QuickFolders.Preferences.getUserStyle("HoveredTab","color","Black");
+
+		    document.getElementById("dragover-colorpicker").color=QuickFolders.Preferences.getUserStyle("DragTab","background-color", "#E93903"); 
+		    document.getElementById("dragover-fontcolorpicker").color=QuickFolders.Preferences.getUserStyle("DragTab","color", "White"); 
+
+		    document.getElementById("toolbar-colorpicker").color=QuickFolders.Preferences.getUserStyle("Toolbar","background-color", "White"); 
 		    
-		    document.getElementById("activetab-colorpicker").color=QuickFolders.Preferences.getUserStyle("ActiveTab","color"); // this.getElementStyle('.toolbar-flat toolbarbutton.selected-folder', 'background-color');
-		    document.getElementById("hover-colorpicker").color=QuickFolders.Preferences.getUserStyle("HoveredTab","color"); // this.getElementStyle('.toolbar-flat toolbarbutton:hover', 'background-color');
-		    document.getElementById("toolbar-colorpicker").color=QuickFolders.Preferences.getUserStyle("Toolbar","backrgound-color"); // this.getElementStyle('.toolbar', 'background-color');
 	    }
-	    catch(e) { QuickFolders.Util.logToConsole("Quickfolders:" + e); };
+	    catch(e) { 
+		    alert("Error in QuickFolders:\n" + e); 
+		};
 	    
     }
 }

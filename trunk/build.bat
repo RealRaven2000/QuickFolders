6fdb11860021d62dd0038eb023fe4e4137210bfa
bosmeeuw <bosmeeuw@aedb5d07-8755-0410-b22e-9704cff1a143>
2009-01-28 18:59:31 +0000

    Updated build script to exclude .svn folders

--- trunk/build.bat
+++ trunk/build.bat
@@ -1,3 +1,3 @@
 del quickfolders.xpi
-"C:\Program Files\7-Zip\7z" a quickfolders.zip install.rdf chrome.manifest chrome defaults
+"C:\Program Files\7-Zip\7z" a -xr!.svn quickfolders.zip install.rdf chrome.manifest chrome defaults
 rename quickfolders.zip quickfolders.xpi

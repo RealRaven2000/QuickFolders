ddb919eb24a1946163d1b3d7352a15da10acf391
Axel.Grude <Axel.Grude@aedb5d07-8755-0410-b22e-9704cff1a143>
2009-02-14 20:37:43 +0000

    taken out obsolete (?) function call to loadFolderView(0) (this function does not exist and leads to bugs on only some systems)

--- trunk/chrome/content/quickfolders-util.js
+++ trunk/chrome/content/quickfolders-util.js
@@ -12,10 +12,13 @@ QuickFolders.Util = {
     } ,
 
     ensureNormalFolderView: function() {
+	    /*   
+	    this.logToConsole("ensureNormalFolderView...(obsolete?)"); 
+	    // loadFolderView is nowhere to be found!
         if(loadFolderView != undefined) {
             //default folder view to "All folders", so we can select it
             loadFolderView(0);
-        }
+        }*/
     } ,
 
 

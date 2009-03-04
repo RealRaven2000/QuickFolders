fc9fbba5b4e43bcb9413b7127156f8f60c9f2e5a
Axel.Grude <Axel.Grude@aedb5d07-8755-0410-b22e-9704cff1a143>
2009-03-04 16:05:59 +0000

    improved style sheet detection (not only based on title anymore)to make more reliable verbose messages about style sheets in debug mode

--- trunk/chrome/content/qf-styles.js
+++ trunk/chrome/content/qf-styles.js
@@ -1,101 +1,108 @@
 
 QuickFolders.Styles = {
-	
+
 
     getMyStyleSheet: function() {
 	  var styleSheetList = document.styleSheets;
       for (var i=0; i<document.styleSheets.length; i++) {
 	    var ss = document.styleSheets[i];
-	    if (ss.title == "qfStyles") {
+	    if (ss.title == "qfStyles" || ss.href.indexOf("//quickfolders/layout.css")>0) {
+		    QuickFolders.Util.logDebug("stylesheet (" + i + ") [" + ss.title + "] =" + ss.href
+		      + "\nwin.doc.title=" + window.document.title
+		      + "\ndoc.title=" + document.title
+		      + "\nwin.location=" + window.location);
 	      return ss;
         }
+        QuickFolders.Util.logDebug ("stylesheet (" + i + ")"
+          + (ss.title.length ? "[" + ss.title + "]" : "")
+          + " = " + ss.href);
       }
-	  QuickFolders.Util.logToConsole("Can not find style sheet: qfStyles in " +(window.closed ? "closed window" : window.location) );
+	  QuickFolders.Util.logToConsole("Can not find style sheet: qfStyles in " +(window.closed ? "closed window" : window.location)
+	          + "\nwin.doc.title=" + window.document.title
+		        + "\ndoc.documentURI=" + document.documentURI);
 	  return 0;
     },
-    
+
     getElementStyle: function(rule, colortype) {
 	  try {
 	    var ss = this.getMyStyleSheet();
 	    if (!ss || ss==null) {
 		  QuickFolders.Util.logToConsole("Quickfolders: could not find my style sheet!\n")
-	      return;
+	      return "";
         }
-	    
+
 	    var rulesList=ss.cssRules; //  ? ss.cssRules : ss.rules
 	    var i;
 	    var RuleName = '#QuickFolders-Toolbar' + rule;
 	    var rAtoms=RuleName.split(" ");
-	    for (i=1; i<rulesList.length; i++) 
-	    { 
+	    for (i=1; i<rulesList.length; i++)
+	    {
 		    var found;
 		    for (var j=0; j<rAtoms.length; j++) {
 			  found=true;
-			  
+
 		      if (-1 == rulesList[i].selectorText.indexOf(rAtoms[j])) {
 		        found=false;
 		        break;
 	          }
 	        }
 	        if (found) {
-			  var st=rulesList[i].style; // readonly  CSSStyleDeclaration  
+			  var st=rulesList[i].style; // readonly  CSSStyleDeclaration
 		      return st.getPropertyValue(colortype);
 	        }
-		    
+
 	    }
         QuickFolders.Util.logToConsole ("\nQuickFolders.getElementStyle: could not find rule " + RuleName);
       }
       catch(e) {
-	     QuickFolders.Util.logToConsole ("(error)\nQuickFolders.getElementStyle: " + e);   
+	     QuickFolders.Util.logToConsole ("(error)\nQuickFolders.getElementStyle: " + e);
       };
-      return "undefined";
-	    
+      return "";
+
     },
-    
-    setElementStyle: function(rule, colortype, color) {
-	    // alert("setElementStyle!");
-	    // alert("Set color of css class " + rule + " to " + color);
+
+    setElementStyle: function(ss, rule, colortype, color) {
 	    // to do: find elements of this class and change their color
 	    // find the class element itself and change its properties
 	    // persist in options
 	    // load on startup
 	  try {
-	    var ss = this.getMyStyleSheet();
 	    if (!ss || ss==null) {
-		  QuickFolders.Util.logToConsole("could not find my style sheet!\n")
-	      return false;
+		  ss = this.getMyStyleSheet();
+		  if (!ss || ss==null)
+		    return false;
         }
-	    
+
 	    var rulesList=ss.cssRules; //  ? ss.cssRules : ss.rules
 	    var i;
 	    var RuleName = '#QuickFolders-Toolbar' + rule;
 	    var rAtoms=RuleName.split(" ");
-	    for (i=1; i<rulesList.length; i++) 
-	    { 
+	    for (i=1; i<rulesList.length; i++)
+	    {
 		    var found;
 		    for (var j=0; j<rAtoms.length; j++) {
 			  found=true;
-			  
+
 		      if (-1 == rulesList[i].selectorText.indexOf(rAtoms[j])) {
 		        found=false;
 		        break;
 	          }
 	        }
 	        if (found) {
-			  var st=rulesList[i].style; // CSSStyleDeclaration 
+			  var st=rulesList[i].style; // CSSStyleDeclaration
 			  var k;//iterate styles!
-			  
+
 			  for (k=0;k<st.length;k++) {
 				try{
 			      if (colortype==st.item(k)) {
-/*				      
+/*
 			        QuickFolders.Util.logToConsole ("\n=============\nModify item: " + st.item(k)) + " =====================";
 				    QuickFolders.Util.logToConsole ("\nrulesList[i].style[k]=" + rulesList[i].style[k]
 				              + "\nrulesList[i].style[k].parentRule=" + rulesList[i].style.parentRule
 				              + "\nrulesList[i].style.getPropertyPriority=" + rulesList[i].style.getPropertyPriority(colortype)
 				              + "\nst.getPropertyValue(" + colortype + "):" + st.getPropertyValue(colortype)
 				              + "\nrulesList[i].style.getPropertyValue=" + rulesList[i].style.getPropertyValue(colortype));
-*/				   
+*/
 				   st.removeProperty(colortype);
 				   st.setProperty(colortype,color,"important");
 				   break;
@@ -105,14 +112,14 @@ QuickFolders.Styles = {
 		      }
 		      break;
 	        }
-		    
+
 	    }
 	    return true;
       }
       catch(e) {
-	     QuickFolders.Util.logToConsole ("(error) " + e);   
+	     QuickFolders.Util.logToConsole ("(error) " + e);
       };
 	  return false;
     }
-	
+
 }

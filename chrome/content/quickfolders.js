b119d0a02edbc8bdb2c37e8a8e7ea8b781fbd90a
Axel.Grude <Axel.Grude@aedb5d07-8755-0410-b22e-9704cff1a143>
2009-03-05 11:12:14 +0000

    don't display items from "ALWAYS" category when "No Category" is selected Added language specific label in cat dropdown for "Uncategorized" and " "Display All"

--- chrome/content/quickfolders.js
+++ chrome/content/quickfolders.js
@@ -2,37 +2,37 @@
 /*===============
   Project History
   ===============
-  
+
   05/09/2008
     AG added Code to remove dynamic "subfolder" popup menus that act as drop targets. this should also deal with resource issues
        made sorting with mouse more persistant (it sometimes jumped back!)
-       
+
   07/07/2008
     AG added visual indication for drop position (like FX tabs)
-    
+
   10/09/2008
     AG added Quick Help tab (options.xul, options.js)
        removed 'toolbar' style from option window to support tabbed interface
-       
+
   05/10/2008
     AG loading version number dynamically in options dialog
-    
+
   19/11/2008 - Release 0.9.3
     AG fixed bug with drifting popup menus
        fixed same popup not reappearing if first drag not completed
-       
+
   24/11/2008
-    AG rename bug fixed (this used to show original folder name not quickfolder name)  
-    
+    AG rename bug fixed (this used to show original folder name not quickfolder name)
+
   04/01/2009 - Release 0.9.4
     AM added folder category feature
        split sources into more manageable portions
-       
+
   09/01/2009 - Release 0.9.5
     AG added "Always show" category and moved "(All)" to the top
        set category dialog: added [Add] button and made [Ok] button work differently
        re-added addon icon to install.rdf
-       
+
   14/01/2009
     AG Split main options dialog into 3 tabs and added Layout tab
        Added Color Picker to Layout tab (WIP)
@@ -40,15 +40,15 @@
        Added accelerator keys to popup menus
        Layout Improvements in style sheet (saves space)
        Tidied up Project History
-       
+
   17/01/2009 0.9.7
     AG added locale support with help of goofy's sent code
-       coloring of active, hovered tab and tabbar background 
+       coloring of active, hovered tab and tabbar background
        German locale
-       
+
   19/01/2009 0.9.8
     AG storing of colors
-       rounded tabs (flat style)!  
+       rounded tabs (flat style)!
        updated screenshots on the site and added German translations
     Alex added Dutch locale
 
@@ -59,51 +59,55 @@
        fixed an issue with height of category dropdown
        removed (test) color picker from category dialog
        added menu highlighting on drag mail to popup
-    
+
    07/02/2009 0.9.9.1
      AG force display of OK / Cancel button for applying color options
         improved detection of URIs for special icons
 
-   08/02/2009 
+   08/02/2009
      AG update count if total count is displayed
-   
+
    12/02/2009 0.9.9.2
      AG added menu icons
         added gradient display for menu items
 
-   15/02/2009 
+   15/02/2009
      AG+Alex added error handlers for isCorrectWindow and initDelayed functions
-     
+
    17/02/2009
      AG added number of unread messages to popup menu (jump to menu, not drag menu)
-     
-   18/02/2009  
+
+   18/02/2009
      AG do not force switching to All Folders view if not necessary
         added debug mode switch to options
         tidied options layout
 
    26/02/2009
      AG added configurable init Delay and better error logging
-     
+
    04/03/2009
      AG added more focused stylesheet logging
         added an option for changing color of inactive tabs
-     
-   
+
+   05/03/2009
+     AG do not display folders of category "ALWAYS" when "Uncategorized" is selected.
+        for consistency, categories drop down now shows the translated string for "uncategorized"
+
+
   KNOWN ISSUES
   ============
   05/09/2008
     - if folders are added / removed during session this is not refreshed in subfolder list of popup set!
     - folders can be orphaned in config settings and are not removable via interface
-  
-    
+
+
   PLANNED FEATURES
   ================
-    - size of category dropdown should increase with size of 
+    - size of category dropdown should increase with size of
     - colors for categories / single folders {TBD}
-    
+
   WISHLIST
-  ========  
+  ========
     - drag to thread finds quickfolder with correct thread and drops message there
     - multiple lines for quickfolders (probably obsolete, since categories)
     - instantApply support (difficult, as there is no onclose event)
@@ -118,46 +122,46 @@ var QuickFolders = {
        var nDelay = QuickFolders.Preferences.getIntPref('extensions.quickfolders.initDelay');
        if (!nDelay>0) nDelay = 750;
        sWinLocation = new String(window.location);
-       
+
        if(QuickFolders.isCorrectWindow()) {
 		    QuickFolders.Util.logDebug ("initDelayed ==== correct window: " + sWinLocation + " - " + window.document.title + "\nwait " + nDelay + " msec until init()...");
             document.getElementById('QuickFolders-Toolbar').style.display = '';
             setTimeout("QuickFolders.init()", nDelay);
         }
         else {
-	      try { 
-		    document.getElementById('QuickFolders-Toolbar').style.display = 'none'; 
-		    
-		    QuickFolders.Util.logDebug ("DIFFERENT window type(messengerWindow): " 
+	      try {
+		    document.getElementById('QuickFolders-Toolbar').style.display = 'none';
+
+		    QuickFolders.Util.logDebug ("DIFFERENT window type(messengerWindow): "
 		            + document.getElementById('messengerWindow').getAttribute('windowtype')
 		            + "\ndocument.title: " + window.document.title )
-/*		    
+/*
 		    if (sWinLocation.indexOf("options.xul")>0) {
 			  QuickFolders.Util.logDebug ("initDelayed - QF Options Window");
-			  return;  
+			  return;
 		    }
 		    QuickFolders.Util.logDebug ("initDelayed ==== unknown window: " + sWinLocation + " - " + window.document.title);
             setTimeout("QuickFolders.initDelayed()",2500);
-*/            
+*/
           }
 	      catch(e) { ;}
         }
     } ,
-    
+
     isCorrectWindow: function() {
 	    try {
           return document.getElementById('messengerWindow').getAttribute('windowtype') == "mail:3pane";
         }
         catch(e) { return false; }
     } ,
-	
+
     init: function() {
         QuickFolders.Util.logDebug("quickfolders.init()");
-	    
+
         window.addEventListener("keypress", function(e) { QuickFolders.Interface.windowKeyPress(e); }, true);
-        
+
         var folderEntries = QuickFolders.Preferences.getFolderEntries();
-		
+
         if(folderEntries.length > 0) {
             QuickFolders.Model.selectedFolders = folderEntries;
 
@@ -171,22 +175,22 @@ var QuickFolders = {
             QuickFolders.Interface.updateUserStyles();
 
         }
-        
+
         var observerService = Components.classes["@mozilla.org/observer-service;1"].getService(Components.interfaces.nsIObserverService);
-        
+
         observerService.addObserver({
-            observe: function() { 
-	           QuickFolders.Interface.updateFolders(); 
+            observe: function() {
+	           QuickFolders.Interface.updateFolders();
                QuickFolders.Interface.updateUserStyles();
                  }
                },"quickfolders-options-saved", false);
        QuickFolders.Util.logDebug("quickfolders.init() ends.");
     } ,
-	
+
     sayHello: function() {
         alert("Hello from QuickFolders")
     } ,
-	
+
     // handler for dropping folder shortcuts
     toolbarDragObserver: {
         getSupportedFlavours : function () {
@@ -195,13 +199,13 @@ var QuickFolders = {
             flavours.appendFlavour("text/unicode");  // test
             return flavours;
         },
-        
+
         onDragOver: function (evt,flavour,session){
             session.canDrop = true;
         },
-        
+
         onDrop: function (evt,dropData,dragSession) {
-            
+
             switch (dropData.flavour.contentType) {
                 case  "text/x-moz-folder":
                     var sourceUri = QuickFolders.Util.getFolderUriFromDropData(dropData, dragSession)
@@ -216,12 +220,12 @@ var QuickFolders = {
                         myDragPos="LeftMost"
                     else
                         myDragPos="RightMost"
-                    QuickFolders.ChangeOrder.insertAtPosition(dropData.data, "", myDragPos); 
+                    QuickFolders.ChangeOrder.insertAtPosition(dropData.data, "", myDragPos);
                     break;
             }
         }
     } ,
-    
+
     buttonDragObserver: {
         getSupportedFlavours : function () {
             var flavours = new FlavourSet();
@@ -229,21 +233,21 @@ var QuickFolders = {
             flavours.appendFlavour("text/unicode");  // test
             return flavours;
         },
-        
+
         dragOverTimer: null,
-        
+
         onDragEnter: function(evt, dragSession) {
             try {
                 var button = evt.target;
-            
+
                 if(button.tagName == "toolbarbutton") {
                     // highlight drop target
                     if (dragSession.numDropItems==1) {
                       if (dragSession.isDataFlavorSupported("text/unicode" )) {
 	                    // show reordering target position!
 	                    // right or left of current button! (try styling button with > OR < to show on which side the drop will happen)
-	                    var node = dragSession.sourceNode;  
-	                    
+	                    var node = dragSession.sourceNode;
+
 	                    // find out whether drop target button is right or left from source button:
 	                    if (node.hasAttributes()) {
 							var j;
@@ -258,8 +262,8 @@ var QuickFolders = {
 							  }
 	                    }
                       }
-                    } 
-  
+                    }
+
 	                //show context menu if dragged over a button which has subfolders
                     var targetFolder = button.folder;
                     if(targetFolder.hasSubFolders) {
@@ -270,17 +274,17 @@ var QuickFolders = {
                         }
                         if (dragSession.isDataFlavorSupported("text/unicode" ))
 	                      return;  // don't show popup when reordering tabs
-                        
+
                         // instead of using the full popup menu (containing the 3 top commands)
                         // try to create droptarget menu that only contains the target subfolders "on the fly"
-                        // haven't found a way to tidy these up, yet (should be done in onDragExit?) 
+                        // haven't found a way to tidy these up, yet (should be done in onDragExit?)
                         // Maybe they have to be created at the same time as the "full menus" and part of another menu array like menuPopupsByOffset
                         // no menus necessary for folders without subfolders!
 			            var popupset = document.createElement('popupset');
 			            QuickFolders.Interface.getToolbar().appendChild(popupset);
 			            var menupopup = document.createElement('menupopup');
 			            var popupId = 'moveTo_'+targetFolder.URI;
-			            menupopup.setAttribute('id', popupId); 
+			            menupopup.setAttribute('id', popupId);
 			            menupopup.className = 'QuickFolders-folder-popup';
 			            menupopup.folder = targetFolder;
 			            popupset.appendChild(menupopup);
@@ -289,26 +293,26 @@ var QuickFolders = {
 	                    //document.getElementById(popupId).showPopup(button, button.boxObject.screenX, Number(button.boxObject.screenY) + Number(button.boxObject.height));
 	                    // AG fixed, 19/11/2008 - showPopup is deprecated in FX3!
 	                    document.getElementById(popupId).showPopup(button, -1,-1,"context","bottomleft","topleft");
-                        
+
 	                    if (popupId==globalHidePopupId) globalHidePopupId=""; // avoid hiding "itself". globalHidePopupId is not cleared if previous drag cancelled.
-	                    
+
 						/* original by Alex (displays full menu)
                         var popupId = 'QuickFolders-folder-popup-' + targetFolder.URI;
                         var popup = document.getElementById(popupId);
                         popup.showPopup(button,button.boxObject.screenX, Number(button.boxObject.screenY) + Number(button.boxObject.height));
                         */
                     }
-                  
-                    
+
+
                 }
 		        // delete previous drag folders popup!
                 if (globalHidePopupId && globalHidePopupId!="") {
 	                    var popup = document.getElementById(globalHidePopupId);
 				        try {
-					        popup.parentNode.removeChild(popup); //was popup.hidePopup() 
+					        popup.parentNode.removeChild(popup); //was popup.hidePopup()
 				        }
 				        catch (e) {
-					        //window.dump("removing popup: " + globalHidePopupId + " failed!\n" + e + "\n");	
+					        //window.dump("removing popup: " + globalHidePopupId + " failed!\n" + e + "\n");
 				        }
 				        globalHidePopupId="";
                     }
@@ -317,12 +321,12 @@ var QuickFolders = {
                 alert("quickFolders:\n" + e);
             }
         } ,
-        
-        // deal with old folder popups 	
+
+        // deal with old folder popups
         onDragExit: function(event, dragSession) {
 	        var button = event.target;
 	        globalHidePopupId="";
-	        if (dragSession.isDataFlavorSupported("text/unicode" )) 
+	        if (dragSession.isDataFlavorSupported("text/unicode" ))
 	        {
 		        // remove dragdrop marker:
 		        button.className = button.className.replace(/\s*dragLEFT/,"");
@@ -332,63 +336,63 @@ var QuickFolders = {
 		    // problem: event also fires when dragging into the menu, so we can not remove it then!
 	        var targetFolder = button.folder;
 	        var popupId = 'moveTo_'+targetFolder.URI;
-	        
+
 	        // this popup needs to be removed if we drag into another button.
 	        try {
-		        if (document.getElementById(popupId)) 
+		        if (document.getElementById(popupId))
 			        globalHidePopupId = popupId; // arm for hiding! GLOBAL VAR!!
             }
             catch(ex) {
 	            window.dump("Cannot setup for delete: popup " + popupId + "\n" + ex);
             }
 
-	            
+
         } ,
-        
+
 
         onDragOver: function (evt,flavor,session){
             session.canDrop = (flavor.contentType == "text/x-moz-message" || flavor.contentType == "text/unicode");
         },
-        
+
         onDrop: function (evt,dropData,dragSession) {
             var button = evt.target;
             var targetFolder = button.folder;
             globalHidePopupId="";
-            
+
             switch (dropData.flavour.contentType) {
                 case  "text/x-moz-message":  // fall through
                 case  "text/x-moz-folder":
                     var trans = Components.classes["@mozilla.org/widget/transferable;1"].createInstance(Components.interfaces.nsITransferable);
                     trans.addDataFlavor("text/x-moz-message");
-                    
+
                     var messageUris = [];
-                    
+
                     for (var i = 0; i < dragSession.numDropItems; i++) {
                         dragSession.getData (trans, i);
                         var dataObj = new Object();
                         var flavor = new Object();
                         var len = new Object();
                         trans.getAnyTransferData(flavor, dataObj, len);
-                        
+
                         if (flavor.value == "text/x-moz-message" && dataObj) {
                             dataObj = dataObj.value.QueryInterface(Components.interfaces.nsISupportsString);
                             var messageUri = dataObj.data.substring(0, len.value);
-                            
+
                             messageUris.push(messageUri);
                         }
                     }
                     // handler for dropping messages
                     if(messageUris.length > 0) {
                         QuickFolders.Util.moveMessages(
-                          targetFolder, 
+                          targetFolder,
                           messageUris,
                           dragSession.dragAction == nsIDragService.DRAGDROP_ACTION_COPY
                         )
                     }
-                    
+
                     break;
                 case "text/unicode":
-                    QuickFolders.ChangeOrder.insertAtPosition(dropData.data, button.folder.URI, ""); 
+                    QuickFolders.ChangeOrder.insertAtPosition(dropData.data, button.folder.URI, "");
                     break;
             }
         },
@@ -396,12 +400,12 @@ var QuickFolders = {
         onDragStart: function (event, transferData, action) {
             var button = event.target;
             transferData.data = new TransferData();
-            transferData.data.addDataForFlavour("text/unicode", button.folder.URI); // test 
+            transferData.data.addDataForFlavour("text/unicode", button.folder.URI); // test
         }
-        
+
     }
-    
-    
+
+
 }
 
 function MyEnsureFolderIndex(builder, msgFolder)
@@ -409,15 +413,15 @@ function MyEnsureFolderIndex(builder, msgFolder)
     // try to get the index of the folder in the tree
     try {
 	    var index = builder.getIndexOfResource(msgFolder);
-	  
+
 	    if (index == -1) {
 	  	  var parentIndex = MyEnsureFolderIndex(builder, msgFolder.parent);
-	  	
+
 	      // if we couldn't find the folder, open the parent
 	      if(!builder.isContainerOpen(parentIndex)) {
 	            builder.toggleOpenState(parentIndex);
 	      }
-	    
+
 	      index = builder.getIndexOfResource(msgFolder);
 	    }
 	    return index;
@@ -437,7 +441,7 @@ function MySelectFolder(folderUri)
     var folderIndex = MyEnsureFolderIndex(folderTree.builderView, msgFolder);
     // AG no need to switch the view if folder exists in the current one (eg favorite folders or unread Folders
     if (folderIndex<0) {
-       QuickFolders.Util.ensureNormalFolderView();	    
+       QuickFolders.Util.ensureNormalFolderView();
        folderIndex = MyEnsureFolderIndex(folderTree.builderView, msgFolder);
     }
     ChangeSelection(folderTree, folderIndex);
@@ -451,9 +455,9 @@ var myFolderListener = {
     OnItemPropertyChanged: function(parent, item, viewString) {  },
     OnItemIntPropertyChanged: function(item, property, oldValue, newValue) {
         try {
-          if (property == "TotalUnreadMessages" || 
+          if (property == "TotalUnreadMessages" ||
               (QuickFolders.Preferences.isShowUnreadCount() && property == "TotalMessages")) {  // FolderSize
-	            if(QuickFolders) 
+	            if(QuickFolders)
 	                QuickFolders.Interface.updateFolders();
           }
         }
@@ -465,7 +469,7 @@ var myFolderListener = {
     OnItemEvent: function(item, event) {
         if(event == "FolderLoaded") {  // DeleteOrMoveMsgCompleted
 	        try {
-              if(QuickFolders) 
+              if(QuickFolders)
                 QuickFolders.Interface.onFolderSelected();
             }
             catch(e) {};

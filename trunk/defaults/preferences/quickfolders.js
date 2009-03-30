1bd0e05af5332d449a7ac81f58ec9d6c58407883
Axel.Grude <Axel.Grude@aedb5d07-8755-0410-b22e-9704cff1a143>
2009-03-30 19:25:44 +0000

    added pref for alternative shortcut key (CTRL)

--- trunk/defaults/preferences/quickfolders.js
+++ trunk/defaults/preferences/quickfolders.js
@@ -4,6 +4,7 @@ pref("extensions.quickfolders.showUnreadOnButtons",true);
 pref("extensions.quickfolders.showQuickfoldersLabel",false);
 pref("extensions.quickfolders.showUnreadFoldersBold",true);
 pref("extensions.quickfolders.useKeyboardShortcuts",true);
+pref("extensions.quickfolders.useKeyboardShortcutCTRL",false);
 pref("extensions.quickfolders.showTotalNumber",false);
 pref("extensions.quickfolders.showFoldersWithMessagesItalic",false);
 pref("extensions.quickfolders.showFlatStyle",true);

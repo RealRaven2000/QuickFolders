a67facef684e049c00fdffc1f4de458de97374f4
Axel.Grude <Axel.Grude@aedb5d07-8755-0410-b22e-9704cff1a143>
2009-04-14 12:32:19 +0000

    added option to use CTRL instead of ALT for keyboard shortcuts suppress event bubbling (for better Lightning compatibility)

--- trunk/chrome/content/quickfolders-preferences.js
+++ trunk/chrome/content/quickfolders-preferences.js
@@ -1,10 +1,10 @@
 QuickFolders.Preferences = {
     service: Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch),
-    
+
     isDebug: function () {
-	    return this.service.getBoolPref("extensions.quickfolders.debug"); 
-    }, 
-    
+	    return this.service.getBoolPref("extensions.quickfolders.debug");
+    },
+
 
     setFolderEntries: function(folderEntries) {
         this.service.setCharPref("QuickFolders.folders",JSON.stringify(folderEntries));
@@ -41,6 +41,10 @@ QuickFolders.Preferences = {
         return this.service.getBoolPref("extensions.quickfolders.useKeyboardShortcuts");
     } ,
 
+    isUseKeyboardShortcutsCTRL: function() {
+        return this.service.getBoolPref("extensions.quickfolders.useKeyboardShortcutCTRL");
+    } ,
+
     isShowShortcutNumbers: function() {
         return this.service.getBoolPref("extensions.quickfolders.showShortcutNumber");
     } ,
@@ -60,7 +64,7 @@ QuickFolders.Preferences = {
     isShowToolbarIcons: function() {
         return this.service.getBoolPref("extensions.quickfolders.showIcons");
     } ,
-    
+
     getButtonFontSize: function() {
         return this.service.getCharPref("extensions.quickfolders.buttonFontSize");
     } ,
@@ -72,22 +76,22 @@ QuickFolders.Preferences = {
     getLastSelectedCategory: function() {
         return this.service.getCharPref("extensions.quickfolders.lastSelectedCategory")
     },
-    
+
     getUserStyle: function(sId, sType, sDefault) {
 	    // note: storing color as string in order to store OS specific colors like Menu, Highlight
 	    // usage: getUserStyle("ActiveTab","background-color","HighLight")
 	    // usage: getUserStyle("ActiveTab","color", "HighlightText")
 	    var sStyleName = "QuickFolders." + sId + ".Style." + sType;
 	    var sReturnValue="";
-	    
+
         if(!this.service.prefHasUserValue(sStyleName)) {
 		    sReturnValue=sDefault
         }
         else {
 	        var localPref= this.service.getCharPref(sStyleName);
-	        if(localPref) 
+	        if(localPref)
 			    sReturnValue=localPref;
-	        else 
+	        else
 			    sReturnValue=sDefault
         }
         /* QuickFolders.Util.logToConsole("getUserStyle("+ sId+ ", " + sType + ", " + sDefault +")\n" +
@@ -99,19 +103,19 @@ QuickFolders.Preferences = {
     setUserStyle: function(sId, sType, sValue) {
 	    var sStyleName = "QuickFolders." + sId + ".Style." + sType;
         this.service.setCharPref(sStyleName, sValue);
-	    
+
     },
-    
+
     getInstantApplyPref: function() {
         return this.service.getBoolPref("browser.preferences.instantApply");
     },
-    
+
     setInstantApplyPref: function(b) {
         return this.service.setBoolPref("browser.preferences.instantApply",b);
     },
-    
+
     getIntPref: function(p) {
         return this.service.getIntPref(p);
     }
-    
+
 }

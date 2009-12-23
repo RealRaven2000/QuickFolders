583bb6f285936f806b62e197b66a33c44e540ba2
Axel.Grude <Axel.Grude@aedb5d07-8755-0410-b22e-9704cff1a143>
2009-12-23 12:40:25 +0000

    version 1.7

--- defaults/preferences/quickfolders.js
+++ defaults/preferences/quickfolders.js
@@ -1,13 +1,14 @@
 pref("extensions.{3550f703-e582-4d05-9a08-453d09bdfdc6}.description", "chrome://quickfolders/locale/quickfolders.properties");
 pref("extensions.quickfolders.showShortcutNumber",true);
 pref("extensions.quickfolders.showUnreadOnButtons",true);
-pref("extensions.quickfolders.showQuickfoldersLabel",false);
+pref("extensions.quickfolders.showQuickfoldersLabel",true);
 pref("extensions.quickfolders.showUnreadFoldersBold",true);
 pref("extensions.quickfolders.useKeyboardShortcuts",true);
 pref("extensions.quickfolders.useKeyboardShortcutCTRL",false);
 pref("extensions.quickfolders.showTotalNumber",false);
 pref("extensions.quickfolders.showFoldersWithMessagesItalic",false);
 pref("extensions.quickfolders.showFlatStyle",true);
+pref("extensions.quickfolders.showNativeTabStyle",false);
 pref("extensions.quickfolders.autoFocusPreview",true);
 pref("extensions.quickfolders.buttonFontSize","");
 pref("extensions.quickfolders.lastSelectedCategory","");
@@ -17,3 +18,9 @@ pref("extensions.quickfolders.initDelay", 5000);
 pref("extensions.quickfolders.colorTabStyle",0);
 pref("extensions.quickfolders.showSubfolders",false);
 pref("extensions.quickfolders.queuedFolderUpdateDelay",500);
+pref("extensions.quickfolders.debug.css",false);
+pref("extensions.quickfolders.debug.dnd",false);
+pref("extensions.quickfolders.debug.folders",false);
+pref("extensions.quickfolders.debug.popupmenus",false);
+pref("extensions.quickfolders.debug.events",false);
+

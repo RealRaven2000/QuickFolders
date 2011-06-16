dfed461399f5c95f4ebc05c670405e7d7005cebe
Axel.Grude <Axel.Grude@aedb5d07-8755-0410-b22e-9704cff1a143>
2011-06-16 20:08:27 +0000

    2.7

--- trunk/defaults/preferences/quickfolders.js
+++ trunk/defaults/preferences/quickfolders.js
@@ -7,12 +7,11 @@ pref("extensions.quickfolders.showUnreadOnButtons",true);
 pref("extensions.quickfolders.showQuickfoldersLabel",true);
 pref("extensions.quickfolders.textQuickfoldersLabel","QuickFolders");
 pref("extensions.quickfolders.showUnreadFoldersBold",true);
+pref("extensions.quickfolders.useNavigateShortcuts",true);
 pref("extensions.quickfolders.useKeyboardShortcuts",true);
 pref("extensions.quickfolders.useKeyboardShortcutCTRL",false);
 pref("extensions.quickfolders.showTotalNumber",false);
 pref("extensions.quickfolders.showFoldersWithMessagesItalic",false);
-pref("extensions.quickfolders.showFlatStyle",true);
-pref("extensions.quickfolders.showNativeTabStyle",false);
 pref("extensions.quickfolders.showRecentTab",false);
 pref("extensions.quickfolders.autoFocusPreview",true);
 pref("extensions.quickfolders.showSubfolders",false);
@@ -21,9 +20,11 @@ pref("extensions.quickfolders.showCountInSubFolders",false);
 pref("extensions.quickfolders.enableMenuAlphaSorting",false);
 pref("extensions.quickfolders.useRebuildShortcut", false);
 pref("extensions.quickfolders.rebuildShortcutKey", 'F');
+pref("extensions.quickfolders.showToolIcon",true);
 // Current Folder Toolbar
 pref("extensions.quickfolders.showCurrentFolderToolbar",true);
 // Layout
+pref("extensions.quickfolders.style.theme", "flatTabs");
 pref("extensions.quickfolders.buttonFontSize","");
 pref("extensions.quickfolders.showIcons",false);
 pref("extensions.quickfolders.debug",false);
@@ -32,8 +33,25 @@ pref("extensions.quickfolders.colorTabStyle",0);
 pref("extensions.quickfolders.queuedFolderUpdateDelay",500);
 pref("extensions.quickfolders.transparentButtons", false);
 pref("extensions.quickfolders.transparentToolbar", true);
-pref("extensions.quickfolders.transparentButtons", false);
 pref("extensions.quickfolders.buttonShadows", false);
+pref("extensions.quickfolders.pastelColors", true);
+pref("extensions.quickfolders.style.corners.customizedRadius", false);
+pref("extensions.quickfolders.style.corners.customizedTopRadius", "4px");
+pref("extensions.quickfolders.style.corners.customizedBottomRadius", "0px");
+pref("extensions.quickfolders.style.borders",true);
+// System Colors
+pref("extensions.quickfolders.style.Toolbar.background-color", "transparent");
+pref("extensions.quickfolders.style.ActiveTab.color", "HighlightText");
+pref("extensions.quickfolders.style.ActiveTab.background-color", "Highlight");
+pref("extensions.quickfolders.style.DragOver.color", "#E93903");
+pref("extensions.quickfolders.style.DragOver.background-color", "#FFF");
+pref("extensions.quickfolders.style.DragTab.color", "#FFF");
+pref("extensions.quickfolders.style.DragTab.background-color", "#E93903");
+pref("extensions.quickfolders.style.HoveredTab.color", "#FFF");
+pref("extensions.quickfolders.style.HoveredTab.background-color", "orange");
+pref("extensions.quickfolders.style.InactiveTab.color", "buttontext");
+pref("extensions.quickfolders.style.InactiveTab.background-color", "buttonface");
+
 // Debugging Options
 pref("extensions.quickfolders.debug.firstrun", false);
 pref("extensions.quickfolders.debug.buttonStyle", false);
@@ -51,10 +69,12 @@ pref("extensions.quickfolders.debug.recentFolders.detail",false);
 // New Folder Item
 pref("extensions.quickfolders.dragToCreateFolder.pop3",true);
 pref("extensions.quickfolders.dragToCreateFolder.imap",false);
-pref("extensions.quickfolders.recentfolders.itemCount", 12);
 // Recent Folder Item
+pref("extensions.quickfolders.recentfolders.itemCount", 12);
 pref("extensions.quickfolders.recentfolders.showLabel", true);
 pref("extensions.quickfolders.recentfolders.sortAlphabetical", false);
+pref("extensions.quickfolders.recentfolders.color",1);
+pref("extensions.quickfolders.recentfolders.showIcon",true);
 // new Folder Optimization
 pref("extensions.quickfolders.update.disableMinimal", false);
 

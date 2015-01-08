be9fe2ba521ff40407747614f4e6e1c83327e501
Axel.Grude <Axel.Grude@aedb5d07-8755-0410-b22e-9704cff1a143>
2015-01-08 11:40:31 +0000

    changed license to CC

--- chrome/content/qf-styles.js
+++ chrome/content/qf-styles.js
@@ -1,10 +1,11 @@
+"use strict";
 /* BEGIN LICENSE BLOCK
 
-GPL3 applies.
-For detail, please refer to license.txt in the root folder of this extension
+QuickFolders is released under the Creative Commons (CC BY-ND 4.0)
+Attribution-NoDerivatives 4.0 International (CC BY-ND 4.0) 
+For details, please refer to license.txt in the root folder of this extension
 
 END LICENSE BLOCK */
-"use strict";
 
 
 QuickFolders.Styles = {

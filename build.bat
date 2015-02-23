f58f4801cc5f0d6094ff4258c10c9b58a0ae692b
Axel.Grude <Axel.Grude@aedb5d07-8755-0410-b22e-9704cff1a143>
2015-02-23 18:37:34 +0000

    removed legacy xul and moved css to skin folder

--- build.bat
+++ build.bat
@@ -2,5 +2,5 @@
 set /P quickFoldersRev=<revision.txt
 set /a quickFoldersRev+=1
 echo %quickFoldersRev% > revision.txt
-move *.xpi "..\_Test Versions\3.15\"
-rename quickFolders.zip QuickFolders-tb-pb-sm-3.15pre%quickFoldersRev%.xpi
+move *.xpi "..\_Test Versions\4.0\"
+rename quickFolders.zip QuickFolders-tb-pb-sm-4.0.2pre%quickFoldersRev%.xpi

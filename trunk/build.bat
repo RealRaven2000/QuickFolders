d75cf18eebef2f901b503c462552ed576bada5ee
Axel.Grude <Axel.Grude@aedb5d07-8755-0410-b22e-9704cff1a143>
2015-03-07 11:02:03 +0000

    4.0.3 Release

--- trunk/build.bat
+++ trunk/build.bat
@@ -2,5 +2,5 @@
 set /P quickFoldersRev=<revision.txt
 set /a quickFoldersRev+=1
 echo %quickFoldersRev% > revision.txt
-move *.xpi "..\_Test Versions\4.0\"
-rename quickFolders.zip QuickFolders-tb-pb-sm-4.0.2pre%quickFoldersRev%.xpi
+move *.xpi "..\_Test Versions\4.1\"
+rename quickFolders.zip QuickFolders-tb-pb-sm-4.1pre%quickFoldersRev%.xpi

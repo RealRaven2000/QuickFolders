55ad43506f069476d6d1355b5e530793dd186ba0
Axel.Grude <Axel.Grude@aedb5d07-8755-0410-b22e-9704cff1a143>
2015-06-09 21:46:38 +0000

    Fixed searching for missing emails in SeaMonkey & Postbox

--- chrome/content/quickfolders-search.js
+++ chrome/content/quickfolders-search.js
@@ -1,5 +1,7 @@
 
-
+// to see Postbox doing it,
+// check mailnews\base\search\resources\content\SearchDialog.js
+//   and                                        searchTermOverlay.js
 QuickFolders.SearchDialog = {
   addSearchTerm: function addSearchTerm(searchTerm) {
     const Ci = Components.interfaces, 
@@ -9,6 +11,7 @@ QuickFolders.SearchDialog = {
           typeOperator = Ci.nsMsgSearchOp;
     if (utils.Debug) debugger;
     utils.logDebug('addSearchTerm(attrib = ' + searchTerm.attrib + ')');
+    // let's try and clone the term using the proper session
     let rowIndex = gSearchTermList.getRowCount();
     // from http://mxr.mozilla.org/comm-central/source/mailnews/base/search/content/searchTermOverlay.js#232
     //      onMore() called when the [+] button is clicked on a row (simulate last row)
@@ -18,21 +21,67 @@ QuickFolders.SearchDialog = {
     updateRemoveRowButton();
 
     // the user just added a term, so scroll to it
-    gSearchTermList.ensureIndexIsVisible(rowIndex);
+    // gSearchTermList.ensureIndexIsVisible(rowIndex);
   } ,
   
   onLoad: function(event)  {
-    let util = QuickFolders.Util;
-    util.logDebug('QF-Search Load');
+    const Ci = Components.interfaces,
+          Cc = Components.classes,
+          nsMsgSearchScope = Ci.nsMsgSearchScope,
+          util = QuickFolders.Util;
+    
+    if (QuickFolders.Preferences.isDebug)  debugger;
+    util.logDebug('QF-Search Load'); // this is exectued after SearchDialog.js searchOnLoad() !
     if (window.arguments && window.arguments[0]) {
-      let searchTerms = window.arguments[0].searchTerms || null;
+      let args = window.arguments[0],
+          searchTerms = args.searchTerms || null;
       if (!searchTerms) return;
-      for (let i=0; i<searchTerms.length; i++) {
-        this.addSearchTerm(searchTerms[i]);
+      // set the folder to the server uri
+      let folder = args.folder || null;
+      if (typeof selectFolder != 'undefined' && folder) {
+        selectFolder(folder)
       }
-      // remove first row
-      removeSearchRow(0);
-      --gTotalSearchTerms;
+      else
+        if (typeof updateSearchFolderPicker != 'undefined' && folder) {
+          if (!folder.isServer)
+            updateSearchFolderPicker(folder.URI);
+        }
+        
+      if ((util.Application == 'Postbox' || util.Application == 'SeaMonkey')
+          && 
+          typeof initializeSearchRows == 'function') 
+      {
+        // create a collection from array and pass it to initializeSearchRows
+        let termsArray = args.searchTerms,
+            templateCollection = Cc["@mozilla.org/supports-array;1"].createInstance(Ci.nsICollection),
+            count = termsArray.length;
+        for (let i = 0; i < count; i++) {
+          templateCollection.AppendElement(termsArray[i]);
+        }
+        for (let j = 0; j < count; j++) { // just doing it this way to prove my collection 'works'
+          gSearchSession.appendTerm(templateCollection.QueryElementAt(j, Ci.nsIMsgSearchTerm));
+        }
+        
+        // remove last row?
+        removeSearchRow(0); // possible?
+        --gTotalSearchTerms;
+        // now pass the collection
+        initializeSearchRows(nsMsgSearchScope.offlineMail, templateCollection);
+        initializeBooleanWidgets(); // make sure radiobutton 'all' is selected (from first searchTerm)
+      }
+      else {
+        for (let i=0; i<searchTerms.length; i++) {
+          this.addSearchTerm(searchTerms[i]);
+        }
+        // remove first (empty) row
+        removeSearchRow(0);
+        --gTotalSearchTerms;
+      }
+      // Postbox only ?
+      if (saveSearchTerms && window.gSearchSession) {
+        util.logDebug('Saving search terms... to session: ' + window.gSearchSession);
+        saveSearchTerms(searchTerms, window.gSearchSession);
+      }
     }
     
   }

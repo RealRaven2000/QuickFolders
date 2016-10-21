f615608a16b76ad009dc5ddcb86e0d48a9492780
RealRaven2000 <axel.grude@gmail.com>
2016-10-21 13:49:07 +0100

    test commit after creating github clone

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
+# test
 "C:\Program Files\7-Zip\7z" a -xr!.svn quickFolders.zip install.rdf chrome.manifest chrome defaults license.txt
 set /P quickFoldersRev=<revision.txt
 set /a quickFoldersRev+=1

a15a128511ff51ca763134389000904e7c494ca0
RealRaven2000 <axel.grude@gmail.com>
2016-11-26 19:55:46 +0000

    rolled out "pro user" scripts to all pages.

---
+++ js/premium.js
@@ -0,0 +1,35 @@
+
+/* functions that remove elements depending on the user type (from user=pro querystring ) */
+
+	function getQueryVariable(variable)	{
+		var query = window.location.search.substring(1),
+				vars = query.split("&");
+		for (var i=0;i<vars.length;i++) {
+			var pair = vars[i].split("=");
+			if (pair[0] == variable) 
+				return pair[1];
+		}
+		return(null);
+	}
+
+	function removeClassItems(name) {
+		var dbuttons = document.getElementsByClassName(name);
+		for (var i=dbuttons.length-1; i>=0; i--) {
+			// dbuttons[i].style.display='none';
+			dbuttons[i].parentNode.removeChild(dbuttons[i]);
+		}
+	}
+	
+	document.addEventListener("DOMContentLoaded", function(event) { 
+		var user = getQueryVariable("user");
+		if (typeof user!='undefined') {
+			if (user=='pro') {
+				removeClassItems('donateButton');
+				removeClassItems('QuickFoldersFreeUser');
+			}
+			else
+				removeClassItems('QuickFoldersProUser');
+		}
+	});
+
+	

342d0bd0757385b65d1845bfad6815f54bc2738a
RealRaven2000 <axel.grude@gmail.com>
2020-06-28 09:54:21 +0100

    mail extension experiment
    preparation for ESR 78 - mail extension experiments are a necessary interim step for the native API conversion 2021.
    
    Bumping version number to 5.0 in order to back port any bug fixes to Tb68 - the Thunderbird 68  version will remain in the master branch for now

--- build.bat
+++ build.bat
@@ -6,8 +6,8 @@ REM replace previous rev with new
 pwsh -Command "(gc -en UTF8NoBOM install.rdf) -replace 'pre%oldRev%'.trim(), 'pre%quickFoldersRev%' | Out-File install.rdf"
 pwsh -Command "(gc -en UTF8NoBOM manifest.json) -replace 'pre%oldRev%', 'pre%quickFoldersRev%' | Out-File manifest.json"
 rem "C:\Program Files\7-Zip\7z" a -xr!.svn quickFolders.zip install.rdf chrome.manifest chrome defaults license.txt
-"C:\Program Files\7-Zip\7z" a -xr!.svn QuickFoldersWeb.zip manifest.json install.rdf chrome.manifest chrome defaults license.txt
+"C:\Program Files\7-Zip\7z" a -xr!.svn QuickFoldersMXX.zip manifest.json install.rdf chrome.manifest chrome defaults license.txt
 echo %quickFoldersRev% > revision.txt
-move QuickFolders*.xpi "..\..\Release\_Test Versions\4.18\"
+move QuickFolders*.xpi "..\..\Release\_Test Versions\5.0\"
 pwsh -Command "Start-Sleep -m 150"
-rename QuickFoldersWeb.zip QuickFolders-wx-4.18.1pre%quickFoldersRev%.xpi
+rename QuickFoldersMXX.zip QuickFolders-mxx-5.0pre%quickFoldersRev%.xpi

2aacfa1f5ac0a10fa37cabb15b1aade7953fd24a
Klaus Buecher <opto@optosolar.com>
2020-07-10 22:06:55 +0200

    load tools menuitem. action not yet implemented

---
+++ chrome/content/main.js
@@ -0,0 +1,22 @@
+var { gQuicktext } = ChromeUtils.import("chrome://quicktext/content/modules/wzQuicktext.jsm");
+
+var quicktext = {
+  onloadoptions: function ()
+  {
+    window.close();
+  }
+,  
+  onunloadoptions: function ()
+  {
+    this.openSettings();
+  }
+,
+  openSettings: function()
+  {
+    gQuicktext.settingsHandle = window.open("chrome://quicktext/content/settings.xhtml", "quicktextConfig", "chrome,resizable,centerscreen");
+    gQuicktext.settingsHandle.focus();
+  }
+}
+
+// custom event, fired by the overlay loader after it has finished loading
+document.addEventListener("DOMOverlayLoaded_{8845E3B3-E8FB-40E2-95E9-EC40294818C4}", () => { gQuicktext.loadSettings(false); }, { once: true });
--- chrome/content/qf-background.js
+++ chrome/content/qf-background.js
@@ -2,3 +2,47 @@
    background scripts
 */
 console.log("backgr");
+
+
+async function main() {
+
+   // setup ConversionHelper
+   await browser.ConversionHelper.registerChromeUrl([ ["content", "quickfolders", "chrome/content/"] ]);
+   await browser.ConversionHelper.registerApiFolder("chrome://quickfolders/content/api/ConversionHelper/");
+   
+   // define default prefs and migrate legacy settings
+   let defaultPrefs = {
+     "counter": 0,
+     "settingsFolder": "",
+     "defaultImport": "",
+     "menuCollapse": true,
+     "toolbar": true,
+     "popup": false,
+     "keywordKey": "Tab",
+     "shortcutModifier": "alt",
+     "shortcutTypeAdv": false,
+     "collapseState": ""
+   };
+ 
+  // await quicktextPreferences.setDefaults(defaultPrefs);
+  // await quicktextPreferences.migrateFromLegacy(defaultPrefs, "extensions.quicktext.");
+ 
+   await browser.ConversionHelper.setOverlayVerbosity(9);
+ 
+   // register and activate overlays
+   //await browser.ConversionHelper.registerOverlay("chrome://messenger/content/messengercompose/messengercompose.xul", "chrome://quicktext/content/quicktext.xul");        
+   await browser.ConversionHelper.registerOverlay("chrome://messenger/content/messenger.xul", "chrome://quickfolders/content/main.xul");        
+   //await browser.ConversionHelper.registerOverlay("chrome://messenger/content/messengercompose/messengercompose.xhtml", "chrome://quicktext/content/quicktext.xul");        
+   await browser.ConversionHelper.registerOverlay("chrome://messenger/content/messenger.xhtml", "chrome://quickfolders/content/main.xul");        
+   
+   // register a script which is called upon add-on unload (to unload any JSM loaded via overlays)
+  //!! await browser.ConversionHelper.registerUnloadScript("chrome://quickfolders/content/scripts/unload.js");
+   
+   // activate all registered overlays
+   await browser.ConversionHelper.activateOverlays();
+   
+   await browser.ConversionHelper.notifyStartupCompleted();
+ }
+ 
+ main();
+ 

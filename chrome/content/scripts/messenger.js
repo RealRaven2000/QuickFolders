9657a6a9f453bf037c80fb9287a7856b33a006fa
John Bieling <john.bieling@gmx.de>
2020-07-17 14:35:09 +0200

    Update messenger.js

--- chrome/content/scripts/messenger.js
+++ chrome/content/scripts/messenger.js
@@ -1,18 +1,9 @@
-var { myModule } = ChromeUtils.import(this.extension.rootURI.resolve("content/modules/myModule.jsm"));
-
-function restart(window) {
-	window.alert("I should do something now.");
-}
 
 function onLoad(window, wasAlreadyOpen) {
-	myModule.incValue();
-	console.log("onLoad from the MESSENGER script has been called by <"+this.extension.id+">");
-	console.log("onLoad() has been called for already open window: " + wasAlreadyOpen);
-	console.log("onLoad() for: " + window.location.href);
-	console.log("onLoad() sees myModule value : " + myModule.getValue());
-	
+
 	// Get the label using an entry from the i18n locale JSON file in the _locales folder.
-	let label = this.extension.localeData.localizeMessage("menuLabel");
+	// you currently do not have any JSON locales
+	// let label = this.extension.localeData.localizeMessage("menuLabel");
 
 	// Parse a XUL fragment
 	// The oncommand attribute does not know "this", so we need to specify
@@ -20,25 +11,22 @@ function onLoad(window, wasAlreadyOpen) {
 	// JavaScript files as defined in messenger.WindowListener.startListening()
 	// in background.js.
 	let xul = window.MozXULElement.parseXULToFragment(`
-<menuitem id="myMenuEntry"
-  class="menuitem-iconic"
-  oncommand="window.ListenerExampleAddon.restart(window)"
-  label="${label}"/>
-`);
-	
+<menuseparator 
+	id="quickfolders-settings-sep" />
+<menuitem id="quickfolders-settings"
+	label="&qf.toolbar.quickfolders.toolbar;" 
+	oncommand="window.openDialog('chrome://quickfolders/content/settings.xhtml', 'quicktextConfig', 'chrome,resizable,centerscreen')" 
+	class="menu-iconic  menuitem-iconic" />
+`, 
+	["chrome://quickfolders/locale/calendar.dtd"]);
+
 	// Add the parsed fragment to the UI.
-	let menuAccountmgr = window.document.getElementById("menu_accountmgr");
-	menuAccountmgr.parentNode.insertBefore(xul, menuAccountmgr.nextSibling);
+	let refItem = window.document.getElementById("prefSep");
+	menuAccountmgr.parentNode.insertBefore(xul, refItem);
 }
 
 function onUnload(window, isAddOnShutDown) {
-	myModule.incValue();
-	console.log("onUnload() from the MESSENGER script has been called by <"+this.extension.id+">");
-	console.log("onUnload() has been called because of global add-on shutdown: " + isAddOnShutDown);
-	console.log("onUnload() for: " + window.location.href);
-	console.log("onUnload() sees myModule value : " + myModule.getValue());
-
 	// Remove any added elements.
-	let me = window.document.getElementById("myMenuEntry");
-	me.remove();
+	window.document.getElementById("quickfolders-settings-sep").remove();
+	window.document.getElementById("quickfolders-settings").remove();
 }

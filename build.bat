188bf95f8ac4a8de680993c487a8b643f67b4ed5
RealRaven2000 <axel.grude@gmail.com>
2020-07-25 20:50:58 +0100

    fresh fork for Tb78 - removed shim code.

--- build.bat
+++ build.bat
@@ -3,11 +3,10 @@ set /P quickFoldersRev=<revision.txt
 set /a oldRev=%quickFoldersRev%
 set /a quickFoldersRev+=1
 REM replace previous rev with new
-pwsh -Command "(gc -en UTF8NoBOM install.rdf) -replace 'pre%oldRev%'.trim(), 'pre%quickFoldersRev%' | Out-File install.rdf"
 pwsh -Command "(gc -en UTF8NoBOM manifest.json) -replace 'pre%oldRev%', 'pre%quickFoldersRev%' | Out-File manifest.json"
 rem "C:\Program Files\7-Zip\7z" a -xr!.svn quickFolders.zip install.rdf chrome.manifest chrome defaults license.txt
-"C:\Program Files\7-Zip\7z" a -xr!.svn QuickFoldersWeb.zip manifest.json install.rdf chrome.manifest chrome defaults license.txt
+"C:\Program Files\7-Zip\7z" a -xr!.svn QuickFoldersWeb.zip manifest.json chrome.manifest chrome defaults license.txt
 echo %quickFoldersRev% > revision.txt
-move QuickFolders*.xpi "..\..\Release\_Test Versions\4.18\"
+move QuickFolders*.xpi "..\..\..\Release\_Test Versions\5.0\"
 pwsh -Command "Start-Sleep -m 150"
-rename QuickFoldersWeb.zip QuickFolders-wx-4.18.1pre%quickFoldersRev%.xpi
+rename QuickFoldersWeb.zip QuickFolders-wx-5.0pre%quickFoldersRev%.xpi
--- chrome/content/shimECMAold/quickfolders-shimEcma.js
+++
@@ -1,7 +0,0 @@
-// OLD CODE
-
-QuickFolders.Util.getOrCreateFolder = function dummy() {
-	alert("getOrCreateFolder() - this function is not supported because you are running an older platform version of Mozilla / Gecko.")
-};
-
-

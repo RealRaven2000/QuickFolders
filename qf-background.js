189f7cd902254c756f0802ff634ba59e2d1431b8
Klaus Buecher <kbuecher@optosolar.com>
2020-08-27 22:45:45 +0200

    onboarding/upboarding page

--- qf-background.js
+++ qf-background.js
@@ -4,6 +4,40 @@
  */
 
 async function main() {
+
+
+
+    messenger.runtime.onInstalled.addListener(async ({ reason, temporary }) => {
+        // if (temporary) return; // skip during development
+         switch (reason) {
+           case "install":
+             {
+               const url = browser.runtime.getURL("popup/installed.html");
+               //await browser.tabs.create({ url });
+               await browser.windows.create({ url, type: "popup", height: 600, width: 600, });
+             }
+             break;
+           // see below
+           case "update":
+             {
+               const url = browser.runtime.getURL("popup/update.html");
+               //await browser.tabs.create({ url });
+               await browser.windows.create({ url, type: "popup", height: 600, width: 600, });
+             }
+             break;
+           // see below
+           }
+       });
+      
+      
+      
+      
+   
+
+
+
+
+
     messenger.WindowListener.registerDefaultPrefs("chrome/content/scripts/quickfoldersDefaults.js");
     
     messenger.WindowListener.registerChromeUrl([ 

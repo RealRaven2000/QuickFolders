9d1ba3c54708b904c4aff90204a0e716af5e9348
Klaus Buecher <kbuecher@optosolar.com>
2020-09-12 22:05:20 +0200

    get {addon}, {version} from legacy code

--- popup/update.js
+++ popup/update.js
@@ -26,6 +26,20 @@ addEventListener("click", async (event) => {
   });  
 
 
+//let body = window.document.getElementById("body");
+let doc=document;
+//console.log (document.body.innerText);
+
+addEventListener("load", async (event) => {
+	debugger;
+	let text= document.body.innerHTML;//	console.log (document.body.innerText);
+	let htmltext=text.replace(/{addon}/g, await messenger.Utilities.getAddonName());
+	let  htmltext2=htmltext.replace(/{version}/g, await messenger.Utilities.getAddonVersion());
+	//same for license, TB version   let htmltext=text.replace(/{addon}/g, await messenger.Utilities.getAddonName());
+		document.body.innerHTML=htmltext2;
+
+  });  
+
   loglic();
 
 

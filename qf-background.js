1e1cdac71a07f2f897bcbb623bec7f545f9b634d
RealRaven2000 <axel.grude@gmail.com>
2021-05-24 14:50:01 +0100

    Use Promise for background code instead of WebExtStartupFinished

--- qf-background.js
+++ qf-background.js
@@ -6,75 +6,85 @@
 import * as util from "./scripts/qf-util.mjs.js";
 import {Licenser} from "./scripts/Licenser.mjs.js";
 
-async function main() { 
-  var currentLicense;
-
-  /* There is a generell race condition between onInstall and our main() startup:
+var currentLicense;
+var startupFinished = false;
+var callbacks = [];
+  /* startupFinished: There is a general race condition between onInstall and our main() startup:
    * - onInstall needs to be registered upfront (otherwise we might miss it)
    * - but onInstall needs to wait with its execution until our main function has
    *   finished the init routine
    * -> emit a custom event once we are done and let onInstall await that
    */
-  var startupFinished = false;
-  function emitStartupFinished() {
-    startupFinished = true;
-    const event = new CustomEvent("WebExtStartupFinished");
-    window.dispatchEvent(event);
-  }
+
+messenger.runtime.onInstalled.addListener(async (data) => {
+  let { reason, temporary } = data,
+      isDebug = await messenger.LegacyPrefs.getPref("extensions.quickfolders.debug");
   
-  messenger.runtime.onInstalled.addListener(async (data) => {
-    let { reason, temporary } = data;
-    
-    // Wait until the main startup routine has finished!
-    await new Promise((resolve) => {
-      window.addEventListener("WebExtStartupFinished", resolve, { once: true });
-      if (startupFinished) {
-        // Looks like we missed the one send by main()
-        emitStartupFinished();
-      }
-    });
-    console.log("Startup has finished");
-
-    // if (temporary) return; // skip during development
-    switch (reason) {
-      case "install":
-      {
-        let url = browser.runtime.getURL("popup/installed.html");
-        //await browser.tabs.create({ url });
-        await browser.windows.create({ url, type: "popup", width: 910, height: 750, });
-      }
-      break;
-      // see below
-      case "update":
-      {
-        let currentLicenseInfo = currentLicense.info;
-        if (currentLicenseInfo.status == "Valid") {
-          // suppress update popup for users with licenses that have been recently renewed
-          let gpdays = currentLicenseInfo.licensedDaysLeft;
-          console.log("Licensed - " + gpdays  + " Days left.");
-          // if (gpdays>40) {
-            // console.log("Omitting update popup!");
-            // return;
-          // }
-        }
-        
-
-        let url = browser.runtime.getURL("popup/update.html");
-        //await browser.tabs.create({ url });
-        let screenH = window.screen.height,
-            windowHeight = (screenH > 870) ? 870 : screenH;  // https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/windows/create
-        await browser.windows.create({ url, type: "popup", width: 1000, height: windowHeight, allowScriptsToClose: true,});
-      }
-      break;
-    // see below
-    }    
+  // Wait until the main startup routine has finished!
+  await new Promise((resolve) => {
+    if (startupFinished) {
+      if (isDebug) console.log("QuickFolders - startup code finished.");
+      // Looks like we missed the one send by main()
+      resolve();
+    }
+    callbacks.push(resolve);
   });
-  
-  let key = await messenger.LegacyPrefs.getPref("extensions.quickfolders.LicenseKey");
-  let forceSecondaryIdentity = await messenger.LegacyPrefs.getPref("extensions.quickfolders.licenser.forceSecondaryIdentity");
-  currentLicense = new Licenser(key, { forceSecondaryIdentity });
-  await currentLicense.validate();
+  if (isDebug) {
+    console.log("Startup has finished");
+    console.log("QuickFolders - currentLicense", currentLicense);
+  }
 
+  switch (reason) {
+    case "install":
+    {
+      let url = browser.runtime.getURL("popup/installed.html");
+      await browser.windows.create({ url, type: "popup", width: 910, height: 750, });
+    }
+    break;
+    // see below
+    case "update":
+    {
+      let currentLicenseInfo = currentLicense.info;
+      if (currentLicenseInfo.status == "Valid") {
+        // suppress update popup for users with licenses that have been recently renewed
+        let gpdays = currentLicenseInfo.licensedDaysLeft,
+            isLicensed = (currentLicenseInfo.status == "Valid");
+        if (isLicensed) {
+          if (isDebug) console.log("QuickFolders License - " + gpdays  + " Days left.");
+        }
+        // if (gpdays>40) {
+          // console.log("Omitting update popup!");
+          // return;
+        // }
+      }
+
+      let url = browser.runtime.getURL("popup/update.html");
+      let screenH = window.screen.height,
+          windowHeight = (screenH > 870) ? 870 : screenH;  
+      await browser.windows.create({ url, type: "popup", width: 1000, height: windowHeight, allowScriptsToClose: true,});
+    }
+    break;
+  // see below
+  }    
+});
+
+async function main() {
+  const legacy_root = "extensions.quickfolders.";
+  let key = await messenger.LegacyPrefs.getPref(legacy_root + "LicenseKey"),
+      forceSecondaryIdentity = await messenger.LegacyPrefs.getPref(legacy_root + "licenser.forceSecondaryIdentity"),
+      isDebug = await messenger.LegacyPrefs.getPref(legacy_root + "debug"),
+      isDebugLicenser = await messenger.LegacyPrefs.getPref(legacy_root + "debug.premium.licenser");
+      
+  currentLicense = new Licenser(key, { forceSecondaryIdentity, debug: isDebugLicenser });
+  await currentLicense.validate();
+  
+  // All important stuff has been done.
+  // resolve all promises on the stack
+  if (isDebug) console.log("Finished setting up license startup code");
+  callbacks.forEach(callback => callback());
+  startupFinished = true;
+
+  // listeners for splash pages
   messenger.runtime.onMessage.addListener(async (data, sender) => {
     if (data.command) {
       switch (data.command) {
@@ -85,7 +95,7 @@ async function main() {
   });
   
   messenger.NotifyTools.onNotifyBackground.addListener(async (data) => {
-    let isLog = await messenger.LegacyPrefs.getPref("extensions.quickfolders.debug.notifications");
+    let isLog = await messenger.LegacyPrefs.getPref(legacy_root + "debug.notifications");
     if (isLog && data.func) {
       console.log ("=========================\n" +
                    "BACKGROUND LISTENER received: " + data.func + "\n" +
@@ -154,16 +164,18 @@ async function main() {
         break;
 
       case "updateLicense":
-        let forceSecondaryIdentity = await messenger.LegacyPrefs.getPref("extensions.quickfolders.licenser.forceSecondaryIdentity");
+        let forceSecondaryIdentity = await messenger.LegacyPrefs.getPref(legacy_root + "licenser.forceSecondaryIdentity"),
+            isDebugLicenser = await messenger.LegacyPrefs.getPref(legacy_root + "debug.premium.licenser");
+            
         // we create a new Licenser object for overwriting, this will also ensure that key_type can be changed.
-        let newLicense = new Licenser(data.key, { forceSecondaryIdentity });
+        let newLicense = new Licenser(data.key, { forceSecondaryIdentity, debug: isDebugLicenser });
         await newLicense.validate();
         // Check new license and accept if ok.
         // You may return values here, which will be send back to the caller.
         // return false;
         
         // Update background license.
-        await messenger.LegacyPrefs.setPref("extensions.quickfolders.LicenseKey", newLicense.info.licenseKey);
+        await messenger.LegacyPrefs.setPref(legacy_root + "LicenseKey", newLicense.info.licenseKey);
         currentLicense = newLicense;
         // Broadcast
         messenger.NotifyTools.notifyExperiment({licenseInfo: currentLicense.info})
@@ -186,8 +198,8 @@ async function main() {
   messenger.WindowListener.registerWindow("chrome://messenger/content/customizeToolbar.xhtml", "chrome/content/scripts/qf-customizetoolbar.js");
   messenger.WindowListener.registerWindow("chrome://messenger/content/messageWindow.xhtml", "chrome/content/scripts/qf-messageWindow.js");  
 
-  messenger.WindowListener.registerStartupScript("chrome/content/scripts/qf-startup.js");
-  messenger.WindowListener.registerShutdownScript("chrome/content/scripts/qf-shutdown.js");
+  // messenger.WindowListener.registerStartupScript("chrome/content/scripts/qf-startup.js");
+  // messenger.WindowListener.registerShutdownScript("chrome/content/scripts/qf-shutdown.js");
 
  /*
   * Start listening for opened windows. Whenever a window is opened, the registered
@@ -196,8 +208,6 @@ async function main() {
   * the parameter of startListening(). This object also contains an extension member.
   */
   messenger.WindowListener.startListening(); 
-  
-  emitStartupFinished();
 }
 
 main();

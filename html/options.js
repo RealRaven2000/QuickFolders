af4befffc3e043eaab65172dd7538e611223d8ae
RealRaven2000 <axel.grude@gmail.com>
2021-12-06 22:30:22 +0000

    added Legacy Prefs support (read and write)

--- html/options.js
+++ html/options.js
@@ -1,4 +1,5 @@
 
+
 // add event listeners for tabs
 const activateTab = (event) => {
   const tabSheets = document.querySelectorAll('.tabcontent-container section');
@@ -39,6 +40,91 @@ function openTab(evt, cityName) {
   evt.currentTarget.className += " active";
 } 
 
-document.addEventListener('DOMContentLoaded', () => {
-  i18n.updateDocument();
-}, { once: true });
+
+async function savePref(event) {
+  let target = event.target,
+      prefName = target.dataset.prefName; // automatically added from data-pref-name
+  
+	if (target instanceof HTMLInputElement) {
+		if (target.getAttribute("type") === "checkbox") {
+			await browser.LegacyPrefs.setPref(prefName, target.checked);
+		} 
+    else if (target.getAttribute("type") === "text" ||
+			target.dataset.prefType === "string"
+		) {
+			await browser.LegacyPrefs.setPref(prefName, target.value);
+		} 
+    else if (target.getAttribute("type") === "number") {
+			await browser.LegacyPrefs.setPref(prefName, parseInt(target.value, 10));
+		} 
+    else {
+			console.error("Received change event for input element with unexpected type", event);
+		}
+	} 
+  else if (target instanceof HTMLSelectElement) {
+		if (target.dataset.prefType === "string") {
+			await browser.LegacyPrefs.setPref(prefName, target.value);
+		} 
+    else {
+			await browser.LegacyPrefs.setPref(prefName, parseInt(target.value, 10));
+		}
+	} 
+  else if (element instanceof HTMLTextAreaElement) {
+    await browser.LegacyPrefs.setPref(prefName, target.value);
+  }  
+  else {
+		console.error("Received change event for unexpected element", event);
+	}  
+}
+
+async function loadPrefs() {
+  // use LegacyPrefs
+	const prefElements = Array.from(document.querySelectorAll("[data-pref-name]"));
+	for (const element of prefElements) {
+		const prefName = element.dataset.prefName;
+		if (!prefName) {
+			console.error("Preference element has unexpected data-pref attribute", element);
+			continue;
+		}
+		if (element instanceof HTMLInputElement) {
+      if (element.getAttribute("type") === "checkbox") {
+        element.checked = await browser.LegacyPrefs.getPref(prefName);
+      } 
+      else if (element.getAttribute("type") === "text" ||
+        element.dataset.prefType === "string"
+      ) {
+        element.value = await browser.LegacyPrefs.getPref(prefName);
+      } 
+      else if (element.getAttribute("type") === "number") {
+        element.value = (await browser.LegacyPrefs.getPref(prefName)).toString();
+      } 
+      else {
+        console.error("Input element has unexpected type", element);
+      }
+		} 
+    else if (element instanceof HTMLSelectElement) {
+			if (element.dataset.prefType === "string") {
+				element.value = await browser.LegacyPrefs.getPref(prefName);
+			} else {
+				element.value = (await browser.LegacyPrefs.getPref(prefName)).toString();
+			}
+		} else if (element instanceof HTMLTextAreaElement) {
+      element.value = await browser.LegacyPrefs.getPref(prefName);
+    }
+    else {
+			console.error("Unexpected preference element", element);
+		}
+    
+    // Wire up individual event handlers
+    element.addEventListener("change", savePref);
+    
+	}  
+  
+}
+
+i18n.updateDocument();
+loadPrefs();
+
+
+
+

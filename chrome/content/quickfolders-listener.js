9225b55f53f8eadb844888a5a78bc980195faf6e
RealRaven2000 <axel.grude@gmail.com>
2022-06-08 22:59:11 +0100

    new folder listener

--- chrome/content/quickfolders-listener.js
+++ chrome/content/quickfolders-listener.js
@@ -1,5 +1,8 @@
 "use strict";
 // this will be initialized in qf-messenger.js
 // now register myself as a listener on every mail folder
-var QuickFolders_mailSession = Components.classes["@mozilla.org/messenger/services/session;1"].getService(Components.interfaces.nsIMsgMailSession);
+var { MailServices } = ChromeUtils.import("resource:///modules/MailServices.jsm");
+// Components.classes["@mozilla.org/messenger/services/session;1"].getService(Components.interfaces.nsIMsgMailSession);
+var QuickFolders_mailSession = MailServices.mailSession; 
+
 

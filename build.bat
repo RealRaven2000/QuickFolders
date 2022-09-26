35f92552dd647d2033e8a5d93ed0bec326ef8ca0
RealRaven2000 <axel.grude@gmail.com>
2022-09-26 10:55:21 +0100

    5.13 release

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@ set /P quickFoldersRev=<revision.txt
 set /a oldRev=%quickFoldersRev%
 set /a quickFoldersRev+=1
 REM replace previous rev with new
-powershell -Command "(gc -en UTF8 manifest.json) -replace 'pre%oldRev%', 'pre%quickFoldersRev%' | Out-File manifest.json"
+powershell -Command "(gc -en UTF8 manifest.json) -replace 'pre%oldRev%', 'pre%quickFoldersRev%' | Out-File manifest.json  -encoding utf8"
 "C:\Program Files\7-Zip\7z" a -xr!.svn QuickFoldersWeb.zip manifest.json _locales scripts chrome html popup license.txt *.js *.html
 echo %quickFoldersRev% > revision.txt
 move QuickFolders*.xpi "..\..\..\Release\_Test Versions\5.13\"

6e49e15a5ddcd07bde6fadba564295fbb8549696
RealRaven2000 <axel.grude@gmail.com>
2023-07-15 00:29:20 +0100

    fixed current folder toolbars in multiple folder tabs, tab session support

--- chrome/content/scripts/qf-3pane.js
+++ chrome/content/scripts/qf-3pane.js
@@ -29,117 +29,114 @@ async function onLoad(activatedWhileWindowOpen) {
 
       //------------------------------------ overlay current folder (navigation bar)
       let INJECTED_ELEMENTS =
-`
-      <hbox id="QuickFolders-PreviewToolbarPanel" 
-            class="QuickFolders-NavigationPanel">
-        <span flex="5" id="QF-CurrentLeftSpacer"> </span>
-        <toolbar id="QuickFolders-CurrentFolderTools" class="contentTabToolbar" iconsize="small">
-          <toolbarbutton id="QuickFolders-CurrentMail"
-            class="icon draggable"
-            tooltiptext="__MSG_qf.tooltip.emailIcon__" />
-          <toolbarbutton id="QuickFolders-Recent-CurrentFolderTool" tag="#Recent" class="recent icon"
-            context="QuickFolders-folder-popup-Recent-CurrentFolderTool"
-            position="after_start"
-            oncontextmenu="QuickFolders.Interface.onClickRecentCurrentFolderTools(event.target, event, true); return false;"
-            onclick= "QuickFolders.Interface.onClickRecentCurrentFolderTools(event.target, event, true); return false;"
-            ondragenter="QuickFolders.buttonDragObserver.dragEnter(event);"
-            ondragover="QuickFolders.buttonDragObserver.dragOver(event);"
-            tooltiptext="__MSG_qf.tooltip.RecentFolders__"/>
+`<hbox id="QuickFolders-PreviewToolbarPanel" class="QuickFolders-NavigationPanel">
+  <span flex="5" id="QF-CurrentLeftSpacer"> </span>
+  <toolbar id="QuickFolders-CurrentFolderTools" class="contentTabToolbar" iconsize="small">
+    <toolbarbutton id="QuickFolders-CurrentMail"
+      class="icon draggable"
+      tooltiptext="__MSG_qf.tooltip.emailIcon__" />
+    <toolbarbutton id="QuickFolders-Recent-CurrentFolderTool" tag="#Recent" class="recent icon"
+      context="QuickFolders-folder-popup-Recent-CurrentFolderTool"
+      position="after_start"
+      oncontextmenu="QuickFolders.Interface.onClickRecentCurrentFolderTools(event.target, event, true); return false;"
+      onclick= "QuickFolders.Interface.onClickRecentCurrentFolderTools(event.target, event, true); return false;"
+      ondragenter="QuickFolders.buttonDragObserver.dragEnter(event);"
+      ondragover="QuickFolders.buttonDragObserver.dragOver(event);"
+      tooltiptext="__MSG_qf.tooltip.RecentFolders__"/>
 
-          <toolbarseparator special="qfMsgFolderNavigation" />
+    <toolbarseparator special="qfMsgFolderNavigation" />
 
-          <toolbarbutton id="quickFoldersPreviousUnread"
-            class="icon"
-            special="qfMsgFolderNavigation" 
-            tooltiptext="__MSG_qf.tooltip.goPreviousFolder__"
-            onclick="QuickFolders.Interface.onGoPreviousMsg(this);" />
-          <toolbarbutton id="quickFoldersNavToggle" 
-            special="qfMsgFolderNavigation" 
-            tooltiptext="__MSG_qf.tooltip.quickFoldersNavToggle__"
-            onclick="QuickFolders.Interface.onToggleNavigation(this);" />
-          <toolbarbutton id="quickFoldersNextUnread"
-            class="icon"
-            special="qfMsgFolderNavigation" 
-            tooltiptext="__MSG_qf.tooltip.goNextFolder__"
-            onclick="QuickFolders.Interface.onGoNextMsg(this);" />
-          <toolbarbutton id="QuickFolders-CurrentThread"
-            class="icon"
-            special="qfMsgFolderNavigation" 
-            oncommand="QuickFolders.Interface.onClickThreadTools(event.target, event); return false;"
-            tooltiptext="__MSG_qf.tooltip.conversationRead__" />
-                          
-          <toolbarbutton id="quickFoldersSkipFolder"
-            class="icon"
-            special="qfMsgFolderNavigation" 
-            oncommand="QuickFolders.Interface.onSkipFolder(this);"
-            tooltiptext="__MSG_qf.tooltip.skipUnreadFolder__" />
-          <toolbarseparator id="QuickFolders-Navigate-Separator" />
-          <toolbarbutton id="QuickFolders-NavigateUp"
-            class="icon"
-            onclick="QuickFolders.Interface.goUpFolder();"
-            tooltiptext="__MSG_qf.tooltip.folderUp__"/>
-          <toolbarbutton id="QuickFolders-NavigateLeft"
-            class="icon"
-            onclick="QuickFolders.Interface.goPreviousSiblingFolder();"/>
+    <toolbarbutton id="quickFoldersPreviousUnread"
+      class="icon"
+      special="qfMsgFolderNavigation" 
+      tooltiptext="__MSG_qf.tooltip.goPreviousFolder__"
+      onclick="QuickFolders.Interface.onGoPreviousMsg(this);" />
+    <toolbarbutton id="quickFoldersNavToggle" 
+      special="qfMsgFolderNavigation" 
+      tooltiptext="__MSG_qf.tooltip.quickFoldersNavToggle__"
+      onclick="QuickFolders.Interface.onToggleNavigation(this);" />
+    <toolbarbutton id="quickFoldersNextUnread"
+      class="icon"
+      special="qfMsgFolderNavigation" 
+      tooltiptext="__MSG_qf.tooltip.goNextFolder__"
+      onclick="QuickFolders.Interface.onGoNextMsg(this);" />
+    <toolbarbutton id="QuickFolders-CurrentThread"
+      class="icon"
+      special="qfMsgFolderNavigation" 
+      oncommand="QuickFolders.Interface.onClickThreadTools(event.target, event); return false;"
+      tooltiptext="__MSG_qf.tooltip.conversationRead__" />
+                    
+    <toolbarbutton id="quickFoldersSkipFolder"
+      class="icon"
+      special="qfMsgFolderNavigation" 
+      oncommand="QuickFolders.Interface.onSkipFolder(this);"
+      tooltiptext="__MSG_qf.tooltip.skipUnreadFolder__" />
+    <toolbarseparator id="QuickFolders-Navigate-Separator" />
+    <toolbarbutton id="QuickFolders-NavigateUp"
+      class="icon"
+      onclick="QuickFolders.Interface.goUpFolder();"
+      tooltiptext="__MSG_qf.tooltip.folderUp__"/>
+    <toolbarbutton id="QuickFolders-NavigateLeft"
+      class="icon"
+      onclick="QuickFolders.Interface.goPreviousSiblingFolder();"/>
 
-          <hbox class="folderBarContainer">
-            <toolbarbutton id="QuickFoldersCurrentFolder"
-              label="Current Folder"
-              class="selected-folder"
-              ondragenter="QuickFolders.buttonDragObserver.dragEnter(event);"
-              ondragover="QuickFolders.buttonDragObserver.dragOver(event);"/>
-          </hbox>
+    <hbox class="folderBarContainer">
+      <toolbarbutton id="QuickFoldersCurrentFolder"
+        label="Current Folder"
+        class="selected-folder"
+        ondragenter="QuickFolders.buttonDragObserver.dragEnter(event);"
+        ondragover="QuickFolders.buttonDragObserver.dragOver(event);"/>
+    </hbox>
 
-          <toolbarbutton id="QuickFolders-NavigateRight"
-            class="icon"
-            onclick="QuickFolders.Interface.goNextSiblingFolder();"/>
-          <toolbarseparator id="QuickFolders-Navigate-Separator2" />
-          <toolbarbutton id="QuickFolders-currentFolderMailFolderCommands"
-            class="icon"
-            tooltiptext="__MSG_qf.tooltip.mailFolderCommands__"
-            onclick="QuickFolders.Interface.showCurrentFolderMailContextMenu(event.target);"
-            oncontextmenu="QuickFolders.Interface.showCurrentFolderMailContextMenu(event.target);" 
-            collapsed="true"/>
-          <toolbarbutton id="QuickFolders-RepairFolderBtn"
-            class="icon"
-            tooltiptext="Repair Folder..."
-            oncommand="QuickFolders.Interface.onRepairFolder(null);"
-            tag="qfIconRepairFolders"
-            collapsed="true"/>
-                                        
-          <hbox id="QuickFolders-currentFolderIconCommands" >
-            <toolbarbutton id="QuickFolders-SelectIcon"
-              class="icon"
-              tooltiptext="__MSG_qf.foldercontextmenu.quickfolders.customizeIcon__"
-              oncommand="QuickFolders.Interface.onSelectIcon(this,event);"
-              tag="qfIconAdd"/>
-            <toolbarbutton id="QuickFolders-RemoveIcon"
-              class="icon"
-              tooltiptext="__MSG_qf.foldercontextmenu.quickfolders.removeIcon__"
-              collapsed = "true"
-              oncommand="QuickFolders.Interface.onRemoveIcon(this,event);"
-              tag="qfIconRemove"/>
-          </hbox>
+    <toolbarbutton id="QuickFolders-NavigateRight"
+      class="icon"
+      onclick="QuickFolders.Interface.goNextSiblingFolder();"/>
+    <toolbarseparator id="QuickFolders-Navigate-Separator2" />
+    <toolbarbutton id="QuickFolders-currentFolderMailFolderCommands"
+      class="icon"
+      tooltiptext="__MSG_qf.tooltip.mailFolderCommands__"
+      onclick="QuickFolders.Interface.showCurrentFolderMailContextMenu(event.target);"
+      oncontextmenu="QuickFolders.Interface.showCurrentFolderMailContextMenu(event.target);" 
+      collapsed="true"/>
+    <toolbarbutton id="QuickFolders-RepairFolderBtn"
+      class="icon"
+      tooltiptext="Repair Folder..."
+      oncommand="QuickFolders.Interface.onRepairFolder(null);"
+      tag="qfIconRepairFolders"
+      collapsed="true"/>
+                                  
+    <hbox id="QuickFolders-currentFolderIconCommands" >
+      <toolbarbutton id="QuickFolders-SelectIcon"
+        class="icon"
+        tooltiptext="__MSG_qf.foldercontextmenu.quickfolders.customizeIcon__"
+        oncommand="QuickFolders.Interface.onSelectIcon(this,event);"
+        tag="qfIconAdd"/>
+      <toolbarbutton id="QuickFolders-RemoveIcon"
+        class="icon"
+        tooltiptext="__MSG_qf.foldercontextmenu.quickfolders.removeIcon__"
+        collapsed = "true"
+        oncommand="QuickFolders.Interface.onRemoveIcon(this,event);"
+        tag="qfIconRemove"/>
+    </hbox>
 
-          <toolbarbutton id="QuickFolders-currentFolderFilterActive"
-            class="icon"
-            tooltiptext="__MSG_qf.tooltip.filterStart__"
-            oncommand="QuickFolders.Interface.toggle_FilterMode(!QuickFolders.FilterWorker.FilterMode);" />
-          <toolbarbutton id="QuickFolders-Options"
-            class="icon"
-            tooltiptext="__MSG_qf.menuitem.quickfolders.options__"
-            oncommand="QuickFolders.Interface.viewOptions(-1);"
-            tagName="qfOptions"/>
-          <toolbarbutton id="QuickFolders-Close"
-            class="icon"
-            tooltiptext="__MSG_qf.tooltip.closeToolbar__"
-            oncommand="QuickFolders.Interface.displayNavigationToolbar({display:false});" />
+    <toolbarbutton id="QuickFolders-currentFolderFilterActive"
+      class="icon"
+      tooltiptext="__MSG_qf.tooltip.filterStart__"
+      oncommand="QuickFolders.Interface.toggle_FilterMode(!QuickFolders.FilterWorker.FilterMode);" />
+    <toolbarbutton id="QuickFolders-Options"
+      class="icon"
+      tooltiptext="__MSG_qf.menuitem.quickfolders.options__"
+      oncommand="QuickFolders.Interface.viewOptions(-1);"
+      tagName="qfOptions"/>
+    <toolbarbutton id="QuickFolders-Close"
+      class="icon"
+      tooltiptext="__MSG_qf.tooltip.closeToolbar__"
+      oncommand="QuickFolders.Interface.displayNavigationToolbar({display:false});" />
 
-        </toolbar>
-        
-        <span flex="5" id="QF-CurrentRightSpacer"> </span>
-      </hbox>
-`;
+  </toolbar>
+  
+  <span flex="5" id="QF-CurrentRightSpacer"> </span>
+</hbox>`;
 
 
       switch(contentDoc.URL) {
@@ -183,7 +180,11 @@ async function onLoad(activatedWhileWindowOpen) {
           selector : null
         }
       ); 
-      win.QuickFolders.Interface.updateNavigationBar(contentDoc);
+      let tabInfo;
+      try {
+        tabInfo = contentDoc.defaultView.tabOrWindow.tabNode;
+      } catch(ex) {;}
+      win.QuickFolders.Interface.updateNavigationBar(contentDoc, tabInfo);
       // -- now we have the current folder toolbar, tell quickFilters to inject its buttons:
       window.QuickFolders.Util.notifyTools.notifyBackground({ func: "updateQuickFilters" });
 

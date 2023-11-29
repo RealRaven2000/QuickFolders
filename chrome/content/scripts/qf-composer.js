d6125335185ec21701b782b0422cae562c6655e7
Axel G <axel.grude@gmail.com>
2023-11-29 14:51:57 +0000

    issue #437 - reparied setting to / from via advanced tab props

--- chrome/content/scripts/qf-composer.js
+++ chrome/content/scripts/qf-composer.js
@@ -1,8 +1,12 @@
 Services.scriptloader.loadSubScript("chrome://quickfolders/content/quickfolders.js", window, "UTF-8");
+Services.scriptloader.loadSubScript("chrome://quickfolders/content/quickfolders-preferences.js", window, "UTF-8");
+Services.scriptloader.loadSubScript("chrome://quickfolders/content/quickfolders-util.js", window, "UTF-8");
 Services.scriptloader.loadSubScript("chrome://quickfolders/content/quickfolders-composer.js", window, "UTF-8");
 
-function onLoad(activatedWhileWindowOpen) {
+async function onLoad(activatedWhileWindowOpen) {
   // window.QuickFolders.Util.logDebug('Adding Compose xul...');
+  await window.QuickFolders.composer.startup(); 
+
 }
 
 function onUnload(isAddOnShutDown) {

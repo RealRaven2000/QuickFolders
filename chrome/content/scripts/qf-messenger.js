0069197d4bf5eb934b9b0088ba33739ce1f7e927
Axel G <axel.grude@gmail.com>
2023-11-29 19:29:13 +0000

    remove obsolete icon patch code

--- chrome/content/scripts/qf-messenger.js
+++ chrome/content/scripts/qf-messenger.js
@@ -505,16 +505,4 @@ function onUnload(isAddOnShutDown) {
   }
   
   
-  // remove icon patch
-  let treeView = window.gFolderTreeView;
-  if (treeView) {
-    // remove  custom flags!
-    delete treeView["supportsIcons"];  
-    delete treeView["qfIconsEnabled"];
-    // remove my opwn function and restore the original
-    if (window.QuickFolders.FolderTree.GetCellProperties) {
-      window.gFolderTreeView.getCellProperties = window.QuickFolders.FolderTree.GetCellProperties;
-      delete window.QuickFolders.FolderTree["GetCellProperties"];
-    }
-  }
 }

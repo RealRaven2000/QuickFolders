2e58041caab1a6b05ecdb3ce1e65de06626772f0
RealRaven2000 <axel.grude@gmail.com>
2025-03-19 23:04:09 +0000

    default to showing options in tab instead of dialog

--- chrome/content/scripts/quickfoldersDefaults.js
+++ chrome/content/scripts/quickfoldersDefaults.js
@@ -73,7 +73,7 @@ pref("extensions.quickfolders.tooltips.msgFolderFlags", false);
 pref("extensions.quickfolders.tooltips.screenReaderSuppress", false); // acessibility
 pref("extensions.quickfolders.autoValidateFolders", true); // check if folder exists when clicking a tab
 pref("extensions.quickfolders.treeIconsDelay", 7500); // repair icons [Bug 26566] - deprecated from TB102
-pref("extensions.quickfolders.optionsInTab", false);
+pref("extensions.quickfolders.optionsInTab", true);
 pref("extensions.quickfolders.findRelated.list", ""); // json structure
 pref("extensions.quickfolders.findRelated.pattern","");
 pref("extensions.quickfolders.findRelated.group",0);

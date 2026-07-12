a77e2b9a2b9cdb8d391b6d08914a5cca9dd12b69
RealRaven2000 <axel.grude@gmail.com>
2026-07-13 00:48:31 +0100

    #677 fix loading order for composer

--- chrome/content/scripts/qf-composer.js
+++ chrome/content/scripts/qf-composer.js
@@ -1,7 +1,19 @@
 Services.scriptloader.loadSubScript("chrome://quickfolders/content/quickfolders.js", window, "UTF-8");
-Services.scriptloader.loadSubScript("chrome://quickfolders/content/quickfolders-preferences.js", window, "UTF-8");
-Services.scriptloader.loadSubScript("chrome://quickfolders/content/quickfolders-util.js", window, "UTF-8");
-Services.scriptloader.loadSubScript("chrome://quickfolders/content/quickfolders-composer.js", window, "UTF-8");
+Services.scriptloader.loadSubScript(
+  "chrome://quickfolders/content/quickfolders-util.js",
+  window,
+  "UTF-8"
+);
+Services.scriptloader.loadSubScript(
+  "chrome://quickfolders/content/quickfolders-preferences.js",
+  window,
+  "UTF-8"
+);
+Services.scriptloader.loadSubScript(
+  "chrome://quickfolders/content/quickfolders-composer.js",
+  window,
+  "UTF-8"
+);
 
 async function onLoad(activatedWhileWindowOpen) {
   // window.QuickFolders.Util.logDebug('Adding Compose xul...');
